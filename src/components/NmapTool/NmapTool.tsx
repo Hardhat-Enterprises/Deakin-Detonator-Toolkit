6ddb4763eaf1f7e1e9686db71c769300f35dd222
Jordyn <jordyn@husk.pro>
2022-07-19 12:57:54 +0800

    feat: improve new nmap implementation

--- src/components/NmapTool/NmapTool.tsx
+++ src/components/NmapTool/NmapTool.tsx
@@ -1,7 +1,6 @@
-import { Button, Center, Code, LoadingOverlay, NativeSelect, Stack, Text, TextInput, Title } from "@mantine/core";
+import { Button, LoadingOverlay, NativeSelect, NumberInput, Stack, TextInput, Title } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { Prism } from "@mantine/prism";
-import { IconScan } from "@tabler/icons";
 import { Command } from "@tauri-apps/api/shell";
 import { useState } from "react";
 
@@ -9,9 +8,21 @@ interface FormValuesType {
     ip: string;
     port: string;
     speed: string;
+    scanOption: string;
+    numTopPorts: number;
 }
 
 const speeds = ["T0", "T1", "T2", "T3", "T4", "T5"];
+
+const scanOptions = [
+    "All",
+    "Operating System",
+    "Firewall Status",
+    "Services",
+    "Stealth",
+    "Device Discovery",
+    "Top ports",
+];
 
 const NmapTool = () => {
     const [loading, setLoading] = useState(false);
@@ -22,15 +33,33 @@ const NmapTool = () => {
             ip: "",
             port: "",
             speed: "T3",
+            scanOption: "All",
+            numTopPorts: 100,
         },
     });
 
     const onSubmit = async (values: FormValuesType) => {
         setLoading(true);
-        const args = [values.speed];
+        const args = [`-${values.speed}`];
 
         if (values.port) {
             args.push(`-p ${values.port}`);
+        }
+
+        if (values.scanOption === "All") {
+            args.push("-A");
+        } else if (values.scanOption === "Operating System") {
+            args.push("-O");
+        } else if (values.scanOption === "Firewall Status") {
+            args.push("-sA");
+        } else if (values.scanOption === "Services") {
+            args.push("-sV");
+        } else if (values.scanOption === "Stealth") {
+            args.push("-sN");
+        } else if (values.scanOption === "Device Discovery") {
+            args.push("-sn");
+        } else if (values.scanOption === "Top ports") {
+            args.push("--top-ports", `${values.numTopPorts}`);
         }
 
         args.push(values.ip);
@@ -38,8 +67,18 @@ const NmapTool = () => {
         const command = new Command("nmap", args);
         const handle = await command.execute();
 
+        const stdout = handle.stdout;
+        const stderr = handle.stderr;
+
+        if (stdout && !stderr) {
+            const output = `$ nmap ${args.join(" ")}\n\n${stdout}`;
+            setOutput(output);
+        } else if (stderr && !stdout) {
+            const output = `$ nmap ${args.join(" ")}\n\n${stderr}`;
+            setOutput(output);
+        }
+
         setLoading(false);
-        setOutput(handle.stdout);
     };
 
     const getConsoleOutputElement = () => {
@@ -60,13 +99,17 @@ const NmapTool = () => {
         }
     };
 
+    // Determine if the current scan options are for the top ports.
+    const isTopPortScan = form.values.scanOption === "Top ports";
+
     return (
         <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
             <LoadingOverlay visible={loading} />
             <Stack>
                 <Title>Network port scanner (NMAP)</Title>
                 <TextInput label={"IP or Hostname"} required {...form.getInputProps("ip")} />
-                <TextInput label={"Port"} {...form.getInputProps("port")} />
+                {!isTopPortScan && <TextInput label={"Port"} {...form.getInputProps("port")} />}
+                {isTopPortScan && <NumberInput label={"Number of top ports"} {...form.getInputProps("numTopPorts")} />}
                 <NativeSelect
                     title={"Scan speed"}
                     data={speeds}
@@ -74,6 +117,14 @@ const NmapTool = () => {
                     placeholder={"Pick a scan speed"}
                     description={"Speed of the scan, refer: https://nmap.org/book/performance-timing-templates.html"}
                     {...form.getInputProps("speed")}
+                />
+                <NativeSelect
+                    title={"Scan option"}
+                    data={scanOptions}
+                    required
+                    placeholder={"Pick a scan option"}
+                    description={"Type of scan to perform"}
+                    {...form.getInputProps("scanOption")}
                 />
                 <Button type={"submit"}>Scan</Button>
                 {getConsoleOutputElement()}
