f7251a99c37cee3c98f80fef661f19ef4ed23b04
JiatengTao <74883073+JiatengTao@users.noreply.github.com>
2022-08-06 22:02:49 -0400

    feature: Add spotlight search to quickly navigate across pages (#24)

---
+++ src/components/RouteWrapper.tsx
@@ -0,0 +1,56 @@
+import AboutPage from "../pages/About";
+import { AttackVectors } from "../pages/AttackVectors";
+import ToolsPage from "../pages/Tools";
+import { CVE202141773 } from "./CVE-2021-41773/CVE-2021-41773";
+import NmapTool from "./NmapTool/NmapTool";
+import { ShodanAPITool } from "./Shodan/Shodan-API-Tool";
+import SnmpCheck from "./SmnpCheck/SmnpCheck";
+
+export interface RouteProperties {
+    name: string;
+    path: string;
+    element: JSX.Element;
+}
+
+export const ROUTES: RouteProperties[] = [
+    {
+        name: "Home",
+        path: "/",
+        element: <AboutPage />,
+    },
+    {
+        name: "About",
+        path: "/about",
+        element: <AboutPage />,
+    },
+    {
+        name: "Tools",
+        path: "/tools",
+        element: <ToolsPage />,
+    },
+    {
+        name: "Attack Vectors",
+        path: "/attack-vectors",
+        element: <AttackVectors />,
+    },
+    {
+        name: "Nmap",
+        path: "/tools/nmap",
+        element: <NmapTool />,
+    },
+    {
+        name: "SnmpCheck",
+        path: "/tools/snmpcheck",
+        element: <SnmpCheck />,
+    },
+    {
+        name: "Shodan API tool",
+        path: "/tools/shodan-api-tool",
+        element: <ShodanAPITool />,
+    },
+    {
+        name: "CVE-2021-41773",
+        path: "/attack-vectors/cve-2021-41773",
+        element: <CVE202141773 />,
+    },
+];
---
+++ src/components/Spotlight/Spotlight.tsx
@@ -0,0 +1,52 @@
+import { Input } from "@mantine/core";
+import type { SpotlightAction } from "@mantine/spotlight";
+import { openSpotlight, SpotlightProvider } from "@mantine/spotlight";
+import { IconSearch } from "@tabler/icons";
+import { useNavigate } from "react-router-dom";
+import { RouteProperties, ROUTES } from "../RouteWrapper";
+
+function SpotlightControl() {
+    return (
+        <Input
+            variant="filled"
+            radius="lg"
+            size="sm"
+            icon={<IconSearch size={16} />}
+            placeholder="Quick search here..."
+            onClick={() => openSpotlight()}
+        />
+    );
+}
+
+function Search() {
+    let navigate = useNavigate();
+
+    const mapRouteToSpotlightAction = (route: RouteProperties): SpotlightAction => {
+        return {
+            title: route.name,
+            description: route.path,
+            onTrigger: () => navigate(route.path),
+        };
+    };
+
+    const actions: SpotlightAction[] = ROUTES.map(mapRouteToSpotlightAction);
+
+    return (
+        <SpotlightProvider
+            limit={4}
+            highlightQuery
+            actions={actions}
+            searchIcon={<IconSearch size={18} />}
+            filter={(query, actions) =>
+                actions.filter((action) => action.title.toLowerCase().includes(query.toLowerCase()))
+            }
+            searchPlaceholder="Search..."
+            shortcut="mod + shift + 1"
+            nothingFoundMessage="Nothing found..."
+        >
+            <SpotlightControl />
+        </SpotlightProvider>
+    );
+}
+
+export default Search;
