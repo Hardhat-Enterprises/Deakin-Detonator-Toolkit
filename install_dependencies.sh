865081a0e91e219dc34c88d557d7ca1dff06cd82
PurpleSn0w <hjeffreys@deakin.edu.au>
2022-08-11 15:46:30 +1000

    Added Arm64 Support

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-
+echo "----------------------------------------"
 echo "Starting installation of dependencies..."
-
+echo "----------------------------------------"
 # Update all packages.
 sudo apt update
 sudo apt upgrade -y
@@ -23,19 +23,36 @@ sudo apt install -y mitmproxy \
         libjavascriptcoregtk-4.0-18 \
         libjavascriptcoregtk-4.0-dev \
         libwebkit2gtk-4.1 \
-        libwebkit2gtk-4.1-dev
+        libwebkit2gtk-4.1-dev \
+		cargo \
+		nodejs
 
+echo "----------------------"
 echo "System deps installed."
 echo "Installing rust..."
+echo "----------------------"
 
 # Install rust.
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 
+echo "-----------------------------"
 echo "Rust installation complete..."
+echo "-----------------------------"
+
+echo "-------------------------------"
+echo "Installing NodeSource NodeJS..."
+echo "-------------------------------"
+#Install NodeSource NodeJS
+sudo su <<E0F 
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+exit
+E0F
 
 # Install openssl.
+echo "---------------------"
 echo "Installing openssl..."
+echo "---------------------"
 pushd /tmp
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
 sudo dpkg -i *.deb
@@ -50,13 +67,17 @@ volta setup
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
 
+echo "---------------------------"
 echo "Installing node and yarn..."
+echo "---------------------------"
 # Install node and yarn.
 volta install node
 volta install yarn
 
+echo "----------------------------------"
 echo "Volta install complete"
 echo "Installing project dependencies..."
+echo "----------------------------------"
 
 # Install all yarn deps.
 yarn install
@@ -64,4 +85,6 @@ yarn install
 # Install exploits to their expected location.
 ./install_exploits.sh
 
+echo "----------------------"
 echo "Installation complete!"
+echo "----------------------"
