1d53c6bdc5a0b6ab2047e1b2869859e5f340a4c1
Jordyn <jordyn@husk.pro>
2022-08-16 18:26:01 +1000

    add some fixes for john

--- src/components/JohnTheRipper/JohnTheRipper.tsx
+++ src/components/JohnTheRipper/JohnTheRipper.tsx
@@ -3,6 +3,7 @@ import { useForm } from "@mantine/form";
 import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
+import { writeTextFile, BaseDirectory } from "@tauri-apps/api/fs";
 
 interface FormValuesType {
     filePath: string;
@@ -25,7 +26,6 @@ const scanOptions = [
 const JohnTheRipper = () => {
     const [loading, setLoading] = useState(false);
     const [output, setOutput] = useState("");
-    const [selectedScanOption, setSelectedScanOption] = useState("");
     const [selectedFileTypeOption, setSelectedFileTypeOption] = useState("");
 
     let form = useForm({
@@ -39,33 +39,37 @@ const JohnTheRipper = () => {
     const onSubmit = async (values: FormValuesType) => {
         setLoading(true);
 
-        if (values.hash.length == 0) {
-            const args = [` "${values.fileType}2john ${values.filePath} > /tmp/hashoutput.txt"`];
-            //const command = values.fileType + "2john";
-            const command = "bash -c";
-            console.log(command + args);
+        if (values.hash.length === 0) {
+            const args = [`${values.filePath}`];
+
             try {
-                const output = await CommandHelper.runCommand(command, args);
-                setOutput(output);
+                const result = await CommandHelper.runCommand(`${values.fileType}2john`, args);
+                await writeTextFile("hash.txt", result, { dir: BaseDirectory.Temp });
+                setOutput(result);
             } catch (e: any) {
+                console.log(e);
                 setOutput(e);
             }
 
             try {
-                const args = [`--format=${values.fileType} /tmp/hash.txt`];
-                const output = await CommandHelper.runCommand("john", args);
-                setOutput(output);
+                const args = [`--format=${values.fileType}`, "/tmp/hash.txt"];
+                const result = await CommandHelper.runCommand("john", args);
+                console.log(result);
+                setOutput(output + "\n" + result);
             } catch (e: any) {
+                console.log(e);
                 setOutput(e);
             }
 
             setLoading(false);
         } else {
-            const args = [`--format=${values.fileType} ${values.hash}`];
+            const args = [`--format=${values.fileType}`, `${values.hash}`];
             try {
-                const output = await CommandHelper.runCommand("john", args);
-                setOutput(output);
+                const result = await CommandHelper.runCommand("john", args);
+                console.log(result);
+                setOutput(output + "\n" + result);
             } catch (e: any) {
+                console.log(e);
                 setOutput(e);
             }
 
