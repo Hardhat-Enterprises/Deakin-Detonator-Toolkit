ec63c544340be9f10324d398c04f94996c3122b1
PurpleSn0w <hjeffreys@deakin.edu.au>
2022-08-17 10:26:27 +1000

    Changes to install_dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
+
 echo "----------------------------------------"
 echo "Starting installation of dependencies..."
+echo "Installing NodeSource NodeJS..."
 echo "----------------------------------------"
+#Install NodeSource NodeJS
+sudo su <<E0F 
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+exit
+E0F
+
+echo "---------------------------------------"
+echo "NodeSource NodeJS installation complete"
+echo "Installing APT packages"
+echo "---------------------------------------"
 # Update all packages.
 sudo apt update
 sudo apt upgrade -y
@@ -38,41 +50,25 @@ source "$HOME/.cargo/env"
 
 echo "-----------------------------"
 echo "Rust installation complete..."
-echo "Installing NodeSource NodeJS..."
-echo "-------------------------------"
-#Install NodeSource NodeJS
-sudo su <<E0F 
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
-exit
-E0F
+echo "Installing openssl..."
+echo "-----------------------------"
 
 # Install openssl.
-echo "---------------------"
-echo "Installing openssl..."
-echo "---------------------"
 pushd /tmp
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
 sudo dpkg -i *.deb
 
-echo "Installing volta..."
-wget https://github.com/volta-cli/volta/releases/download/v1.0.8/volta-1.0.8-linux-openssl-1.1.tar.gz -O volta.tar.gz
-tar xfz volta.tar.gz
-sudo mv * /usr/local/bin/
-popd
 
-volta setup
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
 
-echo "---------------------------"
-echo "Installing node and yarn..."
-echo "---------------------------"
-# Install node and yarn.
-volta install node
-volta install yarn
+echo "-----------------------------"
+echo "Openssl installation complete"
+echo "Installing yarn..."
+echo "-----------------------------"
+# Install yarn.
+npm install -g yarn
 
 echo "----------------------------------"
-echo "Volta install complete"
+echo "Yarn install complete"
 echo "Installing project dependencies..."
 echo "----------------------------------"
 
