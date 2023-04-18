33d50112d3f11eb1de70881eb944134cedba565c
hell0karl <127699720+hell0karl@users.noreply.github.com>
2023-04-18 02:47:36 -0400

    added reference and user guide

--- src/components/Sherlock/Sherlock.tsx
+++ src/components/Sherlock/Sherlock.tsx
@@ -1,8 +1,18 @@
-import { Button, LoadingOverlay, Stack, TextInput, Title } from "@mantine/core";
+import { Button, LoadingOverlay, Stack, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
+import { UserGuide } from "../UserGuide/UserGuide";
+
+const title = "Sherlock Tool";
+const description_guide =
+    "Sherlock is a tool used for searching social netowkrs for the certain username. \n\n" +
+    "Steps to use Sherlock: \n" +
+    "Note: For multiple username add a space in between. 'Sherlock John Billy'\n\n" +
+    "Step 1: Input the username you need to search in the input box.\n" +
+    "Step 2: Click Start Searching \n" +
+    "Step 3: View results in the Output block below.";
 
 interface FormValues {
     username: string;
@@ -40,7 +50,7 @@ const Sherlock = () => {
         <form onSubmit={form.onSubmit(onSubmit)}>
             <LoadingOverlay visible={loading} />
             <Stack>
-                <Title>Sherlock</Title>
+                {UserGuide(title, description_guide)}
                 <TextInput label={"Username"} required {...form.getInputProps("username")} />
                 <Button type={"submit"}>Start Searching!</Button>
                 <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
