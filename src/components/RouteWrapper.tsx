cf3aa4a447977b1c9974f4a209c86333be95795d
whirlingbobo <lds@sent.com>
2023-04-28 22:48:56 -0400

    Implemented ffuf

--- src/components/RouteWrapper.tsx
+++ src/components/RouteWrapper.tsx
@@ -30,6 +30,8 @@ import NetcatTool from "./Netcat/Netcat";
 import Sherlock from "./Sherlock/Sherlock";
 import BEDTool from "./BedTool/BEDTool";
 import Dnsrecon from "./Dnsrecon/Dnsrecon";
+import FfufTool from "./Ffuf/FfufTool";
+
 export interface RouteProperties {
     name: string;
     path: string;
@@ -237,6 +239,12 @@ export const ROUTES: RouteProperties[] = [
         path: "/tools/Dnsrecon",
         element: <Dnsrecon />,
         description: "Dnsrecon is a python script that is used to find different hosts",
+    },
+    {
+        name: "FfufTool",
+        path: "/tools/FfufTool",
+        element: <FfufTool />,
+        description: "Fuzzy Brute Force Directory Searching",
     },
 ];
 
