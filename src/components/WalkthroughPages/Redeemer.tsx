51cf5d65f3eb183c064458ed15aa446d339b2b98
plainham <hmz19b@gmail.com>
2023-04-29 09:28:19 -0400

    Redeemer video walkthrough added, with npx prettier done

--- src/components/WalkthroughPages/Redeemer.tsx
+++ src/components/WalkthroughPages/Redeemer.tsx
@@ -1,7 +1,7 @@
 import { VideoPlayer } from "../../pages/Walkthroughs";
 
 const Redeemer = () => {
-    return VideoPlayer('https://www.youtube.com/embed/otJZ6IXZgF0?start=1%22', 'Redeemer');
+    return VideoPlayer("https://www.youtube.com/embed/otJZ6IXZgF0?start=1%22", "Redeemer");
 };
 
 export default Redeemer;
