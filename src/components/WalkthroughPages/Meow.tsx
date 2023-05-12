0db88f436969e29e77795e01687ab8cf130ce18e
plainham <hmz19b@gmail.com>
2023-05-12 12:09:10 -0400

    Hack the Box Meow walkthrough video included of meow HTB, with nox prettier used

--- src/components/WalkthroughPages/Meow.tsx
+++ src/components/WalkthroughPages/Meow.tsx
@@ -1,7 +1,7 @@
 import { VideoPlayer } from "../../pages/Walkthroughs";
 
 const Meow = () => {
-    return VideoPlayer('https://www.youtube.com/embed/AmB6UOwHMWs', 'Meow');
+    return VideoPlayer("https://www.youtube.com/embed/AmB6UOwHMWs", "Meow");
 };
 
 export default Meow;
