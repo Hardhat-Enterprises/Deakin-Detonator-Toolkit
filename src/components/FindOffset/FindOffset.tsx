1addd4ec7310a9c776a07423c0a4dad8ad543153
kpdurst <kpdurst@deakin.edu.au>
2023-05-19 13:15:09 -0400

    UserGuide npx prettier

--- src/components/FindOffset/FindOffset.tsx
+++ src/components/FindOffset/FindOffset.tsx
@@ -3,6 +3,19 @@ import { useForm } from "@mantine/form";
 import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
+import { UserGuide } from "../UserGuide/UserGuide";
+
+const title = "Find offset";
+const description_userguide =
+    "This attack vector acts to find the offset to the instruction pointer in a buffer overflow vulnerable binary.\n\n" +
+    "Further information can be found at: https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=buffer+overflow\n\n" +
+    "Using Find offset:\n" +
+    "Step 1: Input a file directory pathway to the binary.\n" +
+    "       Eg: home/binary\n\n" +
+    "Step 2: Enter the number of chars to send.\n" +
+    "       Eg: 200\n\n" +
+    "Step 3: Click Find offset to commence the tools operation.\n\n" +
+    "Step 4: View the Output block below to view the results of the attack vectors execution.";
 
 interface FormValues {
     pathToBinary: string;
@@ -38,7 +51,7 @@ const FindOffset = () => {
         <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
             <LoadingOverlay visible={loading} />
             <Stack>
-                <Title>Find offset</Title>
+                {UserGuide(title, description_userguide)}
                 <TextInput label={"Path to binary"} required {...form.getInputProps("pathToBinary")} />
                 <NumberInput label={"Number of chars to send"} {...form.getInputProps("count")} />
                 <Button type={"submit"}>Find offset</Button>
