511321761e3da69e4009afa82e29d3ad6be205fd
Mohammed Sharf Uddin <msharfuddin264@gmail.com>
2023-05-21 17:05:59 +1000

    updated code for gobuster GUI

--- src/components/buster/buster.tsx
+++ src/components/buster/buster.tsx
@@ -1,88 +1,60 @@
 import React, { useCallback, useState } from "react";
 import { Button, LoadingOverlay, Stack, TextInput, Title } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
+import { CommandHelper } from "../../utils/CommandHelper";
+
+interface FormValues {
+    url: string;
+    wordlist: string;
+    timeout: number;
+    outputPath: string;
+}
 
 export function Gobuster() {
     const [loading, setLoading] = useState(false);
     const [output, setOutput] = useState("");
-    const [formData, setFormData] = useState({
-        url: "",
-        wordlist: "",
-        timeout: "10",
+    const form = useForm<FormValues>({
+        initialValues: {
+            url: "",
+            wordlist: "",
+            timeout: 10,
+            outputPath: "",
+        },
     });
 
-    const runGobuster = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const onSubmit = useCallback(async () => {
         setLoading(true);
         setOutput("Running Gobuster command...");
 
-        const { url, wordlist, timeout } = formData;
-        const command = `gobuster dir -u ${url} -w ${wordlist} -t ${timeout}`;
+        const { url, wordlist, timeout, outputPath } = form.values;
 
         try {
-            setOutput(`Executing command: ${command}`);
-            const response = await fetch("/run-gobuster", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ command }),
-            });
-
-            if (response.ok) {
-                const stdout = await response.text();
-                setOutput(stdout);
-            } else {
-                const errorMessage = await response.text();
-                setOutput(`Error: ${errorMessage}`);
-            }
+            const args = ["gobuster", "dir", "-u", url, "-w", wordlist, "-t", timeout.toString(), "-o", outputPath];
+            const output = await CommandHelper.runCommand("python3", args);
+            setOutput(output);
         } catch (error) {
-            let errorMessage = "An error occurred";
-            if (error instanceof Error) {
-                errorMessage = error.message;
-            } else if (typeof error === "string") {
-                errorMessage = error;
-            } else if (error && typeof error === "object") {
-                errorMessage = JSON.stringify(error);
-            }
-            setOutput(`Error: ${errorMessage}`);
+            setOutput(`Error: ${(error as Error).message}`);
         }
 
         setLoading(false);
-    };
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-    };
+    }, [form.values]);
 
     const clearOutput = useCallback(() => {
         setOutput("");
-        setFormData({ url: "", wordlist: "", timeout: "10" });
-    }, []);
+        form.reset();
+    }, [form]);
 
     return (
-        <form onSubmit={runGobuster}>
+        <form onSubmit={onSubmit}>
             <LoadingOverlay visible={loading} />
-            <Stack>
+            <Stack spacing="md">
                 <Title>Gobuster</Title>
-                <TextInput label="URL" required name="url" value={formData.url} onChange={handleChange} />
-                <TextInput
-                    label="Wordlist"
-                    required
-                    name="wordlist"
-                    value={formData.wordlist}
-                    onChange={handleChange}
-                />
-                <TextInput
-                    label="Timeout (seconds)"
-                    type="number"
-                    min={1}
-                    name="timeout"
-                    value={formData.timeout}
-                    onChange={handleChange}
-                />
-                <Button type="submit">Run</Button>
+                <TextInput label="URL" required {...form.getInputProps("url")} />
+                <TextInput label="Wordlist" required {...form.getInputProps("wordlist")} />
+                <TextInput label="Timeout (seconds)" type="number" min={1} {...form.getInputProps("timeout")} />
+                <TextInput label="Output Path" required {...form.getInputProps("outputPath")} />
+                <Button type="submit">Run Gobuster</Button>
                 <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
             </Stack>
         </form>
