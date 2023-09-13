1ebcef17ce663e6318a61265b03ca66ff5bc6c8a
AnuragVerma26 <anurag.verma0469@gmail.com>
2023-09-12 21:37:21 -0400

    first commit Monitorstwo

--- src/components/RouteWrapper.tsx
+++ src/components/RouteWrapper.tsx
@@ -57,6 +57,7 @@ import Pilgrimage from "./WalkthroughPages/Pilgrimage";
 import Topology from "./WalkthroughPages/Topology";
 import Fcrackzip from "./Fcrackzip/Fcrackzip";
 import GoBusterTool from "./GobusterTool/Gobuster";
+import Monitorstwo from "./WalkthroughPages/Monitorstwo";
 
 export interface RouteProperties {
     name: string;
@@ -429,6 +430,12 @@ export const ROUTES: RouteProperties[] = [
         path: "/tools/Gobuster",
         element: <GoBusterTool />,
         description: "Gobuster is a tool used to brute force: URIs in websites.",
+    },
+    {
+        name: "Monitorstwo",
+        path: "/walkthroughs/Monitorstwo",
+        element: <Monitorstwo />,
+        description: "A walkthrough on Monitorstwo Hack the Box challenge",
     },
 ];
 
---
+++ src/components/WalkthroughPages/Monitorstwo.tsx
@@ -0,0 +1,10 @@
+import { VideoPlayer } from "../../pages/Walkthroughs";
+
+const Monitorstwo = () => {
+    return VideoPlayer(
+        "https://docs.google.com/document/d/1yKU0yO15Va_TCLTql7GTbSdG0mY-nDkjVNFc8yiNKPQ/edit?usp=sharing",
+        "Monitorstwo"
+    );
+};
+
+export default Monitorstwo;
