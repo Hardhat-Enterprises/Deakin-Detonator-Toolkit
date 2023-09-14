563e32b9660829d7e2479ee5da47e0c67a728e74
Cameron Crabb <camcrabb1@gmail.com>
2023-09-13 21:49:30 -0400

    Aplhabetized tools + Tool Category Filter

--- src/pages/Tools.tsx
+++ src/pages/Tools.tsx
@@ -1,24 +1,62 @@
-import { Stack, Table, Title } from "@mantine/core";
+import { Stack, Table, Title, NativeSelect } from "@mantine/core";
+import { useState } from "react";
 import { getTools } from "../components/RouteWrapper";
 import ToolItem from "../components/ToolItem/ToolItem";
 
 const ToolsPage = () => {
-    const rows = getTools().map((tool) => {
-        return <ToolItem title={tool.name} description={tool.description} route={tool.path} key={tool.name} />;
-    });
+    const categories = [
+        "Network Scanning and Enumeration",
+        "Vulnerability Assessment and Exploitation",
+        "Password Cracking and Authentication Testing",
+        "Web Application Testing",
+        "Attack Tools",
+        "File Analysis and Recovery",
+        "Information Gathering and Analysis",
+        "Miscellaneous",
+    ];
+
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const tools = getTools();
 
     return (
         <Stack align={"center"}>
-            <Title>Tools</Title>
+            <Title>Tools Categories</Title>
+            <NativeSelect
+                value={selectedCategory}
+                onChange={(e) => {
+                    setSelectedCategory(e.target.value);
+                }}
+                title="Filter for a Category"
+                data={categories}
+                required
+                placeholder="Filter for a Category"
+            />
             <Table horizontalSpacing="xl" verticalSpacing="md" fontSize="md">
                 <thead>
                     <tr>
                         <th>Tool name</th>
                         <th>Tool description</th>
-                        <th>Controls</th>
+                        <th>Category</th>
                     </tr>
                 </thead>
-                <tbody>{rows}</tbody>
+
+                <tbody>
+                    {tools.map((tool) => {
+                        // Check if the selected category matches the tool's category
+                        if (!selectedCategory || tool.category === selectedCategory) {
+                            return (
+                                <ToolItem
+                                    title={tool.name}
+                                    description={tool.description}
+                                    route={tool.path}
+                                    category={tool.category}
+                                    key={tool.name}
+                                />
+                            );
+                        }
+                        return null; // Skip rendering if the category doesn't match
+                    })}
+                </tbody>
             </Table>
         </Stack>
     );
