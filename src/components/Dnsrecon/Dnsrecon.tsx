6c434391e17f1ab47ff807092f3c0cd5cf05bd73
Blitzle8264 <87558844+Blitzle8264@users.noreply.github.com>
2023-09-21 04:47:09 +0000

    Update Dnsrecon.tsx
    Added Save output to text file and cancel operation functionalities

--- src/components/Dnsrecon/Dnsrecon.tsx
+++ src/components/Dnsrecon/Dnsrecon.tsx
@@ -4,6 +4,8 @@ import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { UserGuide } from "../UserGuide/UserGuide";
+import { SaveOutputToTextFile } from "../SaveOutputToFile/SaveOutputToTextFile";
+import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
 
 const title = "Dnsrecon";
 const description_userguide =
@@ -23,6 +25,7 @@ interface FormValues {
 export function Dnsrecon() {
     const [loading, setLoading] = useState(false);
     const [output, setOutput] = useState("");
+    const [pid, setPid] = useState("");
 
     let form = useForm({
         initialValues: {
@@ -30,14 +33,57 @@ export function Dnsrecon() {
         },
     });
 
+    /**
+     * handleProcessData: Callback to handle and append new data from the child process to the output.
+     * It updates the state by appending the new data received to the existing output.
+     *
+     * @param {string} data - The data received from the child process.
+     */
+    const handleProcessData = useCallback((data: string) => {
+        setOutput((prevOutput) => prevOutput + "\n" + data); // Append new data to the previous output.
+    }, []);
+
+    /**
+     * handleProcessTermination: Callback to handle the termination of the child process.
+     * Once the process termination is handled, it clears the process PID reference and
+     * deactivates the loading overlay.
+     * @param {object} param0 - An object containing information about the process termination.
+     * @param {number} param0.code - The exit code of the terminated process.
+     * @param {number} param0.signal - The signal code indicating how the process was terminated.
+     */
+    const handleProcessTermination = useCallback(
+        ({ code, signal }: { code: number; signal: number }) => {
+            if (code === 0) {
+                handleProcessData("\nProcess completed successfully.");
+            } else if (signal === 15) {
+                handleProcessData("\nProcess was manually terminated.");
+            } else {
+                handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
+            }
+            // Clear the child process pid reference
+            setPid("");
+            // Cancel the Loading Overlay
+            setLoading(false);
+        },
+        [handleProcessData] // Dependency on the handleProcessData callback
+    );
+
     const onSubmit = async (values: FormValues) => {
         setLoading(true);
 
         const args = ["-d", values.url];
-        const output = await CommandHelper.runCommand("dnsrecon", args);
-
-        setOutput(output);
-        setLoading(false);
+        try {
+            const result = await CommandHelper.runCommandGetPidAndOutput(
+                "dnsrecon",
+                args,
+                handleProcessData,
+                handleProcessTermination
+            );
+            setPid(result.pid);
+            setOutput(result.output);
+        } catch (e: any) {
+            setOutput(e.message);
+        }
     };
 
     const clearOutput = useCallback(() => {
@@ -46,11 +92,12 @@ export function Dnsrecon() {
 
     return (
         <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
-            <LoadingOverlay visible={loading} />
+            {LoadingOverlayAndCancelButton(loading, pid)}
             <Stack>
                 {UserGuide(title, description_userguide)}
                 <TextInput label={"URL"} required {...form.getInputProps("url")} />
                 <Button type={"submit"}>Scan</Button>
+                {SaveOutputToTextFile(output)}
                 <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
             </Stack>
         </form>
