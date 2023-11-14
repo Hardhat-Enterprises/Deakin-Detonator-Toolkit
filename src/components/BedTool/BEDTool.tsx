3b9f22ae1e79b8f9dbad71dc292f49bf7d9cba7b
Sakada <limsakada19@yahoo.com>
2023-11-15 10:53:21 +1100

    Improve the usability and added user-guide to tools

--- src/components/BedTool/BEDTool.tsx
+++ src/components/BedTool/BEDTool.tsx
@@ -8,37 +8,39 @@ import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFil
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
 
 //plugin header selection field
-const plugin_list = ["FTP", "SMTP", "POP", "HTTP", "IRC", "IMAP", "PJL", "LPD", "FINGER", "SOCKS4", "SOCKS5"];
+const plugin_list = ["FTP", "SMTP", "POP", "HTTP", "IRC", "IMAP", "PJL", "LPD", "FINGER", "SOCKS4"];
 //plugin that require Authentication
 const pluginsRequiringAuth = ["FTP", "IMAP", "POP"];
 //plugin that require Email address
 const pluginRequiringEmail = ["SMTP"];
 //plugin that require username
-const pluginsRequiringUsername = ["SOCKS4", "SOCKS5"];
-//const pluginRequiring IP and port
-const pluginsRequiringIP = ["HTTP", "IRC", "PJL", "LPD", "FINGER"];
-
-//Default value for Target IP
-const defaultTarget = "localhost";
+const pluginsRequiringUsername = ["SOCKS4"];
 
 const title = "BEDTool";
-const description_userguide =
-    "BED is a tool created for checking daemons to identify any potential buffer overflows, format strings, " +
-    "and other parameters.\n\nFurther information can be found at: https://www.kali.org/tools/bed/\n\n" +
-    "Using BED:\n" +
-    "Step 1: Select a Plugin to be used for the scan.\n" +
-    "       Eg: HTTP\n\n" +
-    "Step 2: Field in the require field that show on the UI\n" +
-    "       Eg: For HTTP we need to have a target port.\n\n" +
-    "Step 3: Click Scan to commence BED's operation.\n\n" +
-    "Step 4: View the Output block below to view the results of the tools execution. \n\n" +
-    "Optional: For more advance usage, turn on advance mode.";
+
+const description_userguide = `
+BED (Bruteforce Exploit Detector) is a sophisticated tool designed to check network services for a range of security vulnerabilities including buffer overflows and format string weaknesses.
+
+For detailed information, please visit: https://www.kali.org/tools/bed/
+
+Instructions for using BED:
+1. Select a Service: Choose the service to test from the dropdown menu, for instance, "HTTP" to test a web server.
+
+2. Input Required Fields: Fill in the fields that appear based on the selected service. For example, choosing "FTP" will prompt for additional required fields, such as username and password.
+
+3. Custom Configuration (Optional): Activate 'Custom Configuration' to enter a specific target IP address and port number. If this is not enabled, the tool will default to scanning the local machine.
+
+4. Start Scan: Click the 'Scan' button to begin the evaluation.
+
+5. View Results: The results, including any potential vulnerabilities, will be displayed in the output section below after the scan is completed.
+
+Note: The 'Custom Configuration' option is designed for advanced users who wish to target a specific network address or require custom settings. If not used, BED will assume the target is the local host.
+`;
 
 interface FormValues {
     plugin: string;
     target: string;
     port: string;
-    timeout: string;
     email: string;
     username: string;
     password: string;
@@ -50,7 +52,7 @@ export function BEDTool() {
     const [output, setOutput] = useState("");
     const [allowSave, setAllowSave] = useState(false);
     const [hasSaved, setHasSaved] = useState(false);
-    const [checkedAdvanced, setCheckedAdvanced] = useState(false);
+    const [customconfig, setCustomconfig] = useState(false);
 
     // state for selected plugin
     const [selectedPlugin, setSelectedPlugin] = useState("");
@@ -60,7 +62,6 @@ export function BEDTool() {
             plugin: "",
             target: "",
             port: "",
-            timeout: "",
             email: "",
             username: "",
             password: "",
@@ -128,10 +129,8 @@ export function BEDTool() {
         setLoading(true);
         const baseArgs = ["-s", values.plugin];
         const conditionalArgs: string[][] = [
-            pluginsRequiringIP.includes(selectedPlugin)
-                ? ["-t", checkedAdvanced ? values.port : defaultTarget, "-p", values.port]
-                : [],
-            checkedAdvanced ? ["-o", values.timeout] : [],
+            customconfig ? ["-t", values.target, "-p", values.port] : [],
+
             pluginsRequiringAuth.includes(selectedPlugin) || selectedPlugin === "SMTP"
                 ? ["-u", selectedPlugin === "SMTP" ? values.email : values.username]
                 : [],
@@ -174,9 +173,9 @@ export function BEDTool() {
                 {UserGuide(title, description_userguide)}
                 <Switch
                     size="md"
-                    label="Advanced Mode"
-                    checked={checkedAdvanced}
-                    onChange={(e) => setCheckedAdvanced(e.currentTarget.checked)}
+                    label="Manual Network Configuration"
+                    checked={customconfig}
+                    onChange={(e) => setCustomconfig(e.currentTarget.checked)}
                 />
                 <Select
                     label="Plugin"
@@ -186,11 +185,7 @@ export function BEDTool() {
                     value={selectedPlugin}
                     onChange={handlePluginChange}
                 />
-                <TextInput
-                    label={"port -> Port to connect to (default: standard port)"}
-                    required
-                    {...form.getInputProps("port")}
-                />
+
                 {pluginsRequiringAuth.includes(selectedPlugin) && (
                     <>
                         <TextInput
@@ -206,24 +201,26 @@ export function BEDTool() {
                         <TextInput label={"username"} required {...form.getInputProps("username")} />
                     </>
                 )}
-                {checkedAdvanced && (
-                    <>
-                        <TextInput
-                            label={"Target -> Host to check (default: localhost)"}
-                            {...form.getInputProps("target")}
-                        />
-                        <TextInput
-                            label={"Timeout -> seconds to wait after each test (default: 2 seconds)"}
-                            {...form.getInputProps("timeout")}
-                        />
-                    </>
-                )}
                 {pluginRequiringEmail.includes(selectedPlugin) && (
                     <>
                         <TextInput
                             label={"Email address -> abc@example.com"}
                             required
                             {...form.getInputProps("email")}
+                        />
+                    </>
+                )}
+                {customconfig && (
+                    <>
+                        <TextInput
+                            label={"Target -> Host to check (default: localhost)"}
+                            required
+                            {...form.getInputProps("target")}
+                        />
+                        <TextInput
+                            label={"port -> Port to connect to (default: standard port)"}
+                            required
+                            {...form.getInputProps("port")}
                         />
                     </>
                 )}
