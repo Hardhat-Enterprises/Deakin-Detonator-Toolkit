c282d53f4deab842e2fd99913a9a987656bb8365
majore15 <rmateo@deakin.edu.au>
2023-12-14 05:58:10 -0500

    Fix <Unknown Error> when using port scan

--- src/components/Netcat/Netcat.tsx
+++ src/components/Netcat/Netcat.tsx
@@ -61,7 +61,7 @@ const NetcatTool = () => {
         //Switch case
         switch (values.netcatOptions) {
             case "Port Scan": //nc syntax: nc -zv <ip address/hostname> <port range>
-            //addition of -n will not perform any dns or name lookups.
+                //addition of -n will not perform any dns or name lookups.
                 args = [`-zvn`];
                 args.push(`${values.ipAddress}`);
                 args.push(`${values.portNumber}`);
