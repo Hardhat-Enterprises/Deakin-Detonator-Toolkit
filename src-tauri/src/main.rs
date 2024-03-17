19bee35dcf5fae7366e3a9032b6f9b65a84b3640
bradleybogg <bradley@darkapex.au>
2024-03-17 04:47:48 -0400

    Add file_handler module and save_file command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,12 @@
   windows_subsystem = "windows"
 )]
 
+mod file_handler;
+use file_handler::{save_file};
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![save_file])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
