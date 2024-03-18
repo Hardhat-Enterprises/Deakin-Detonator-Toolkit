a33cf3834fe8acc60ce37ea167d77d72e933ae7f
chandelshubham63 <chandelshubham63@gmail.com>
2024-03-18 03:35:57 -0400

    Fix code formatting issues identified during CI testing

--- src/components/DirbTool/DirbTool.tsx
+++ src/components/DirbTool/DirbTool.tsx
@@ -100,7 +100,7 @@ export function DirbTool() {
         }
 
         if (values.ignoreHttpCode) {
-            args.push("-N", values.ignoreHttpCode.toString());  
+            args.push("-N", values.ignoreHttpCode.toString());
         }
 
         if (checkedAdvanced) {
