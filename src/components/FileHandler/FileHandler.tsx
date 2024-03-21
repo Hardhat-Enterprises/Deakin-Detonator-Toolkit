da44fcae0126bf8973048ec330ad07db732813c1
bradleybogg <bradley@darkapex.au>
2024-03-21 06:15:12 -0400

    Add file dropper component and update file handling logic

--- src/components/FileHandler/FileHandler.tsx
+++ src/components/FileHandler/FileHandler.tsx
@@ -1,12 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
 
-class FileHandler {
-    generateFileName(name: string): string {
-        const dateTime = new Date().toISOString().replace(/[-:.]/g, '');
-        const uniqueString = uuidv4();
-        const fileName = `${name}_${dateTime}_${uniqueString}`;
-        return fileName;
-    }
+export function generateFileName(fileName: string): string {
+    const dateTime = new Date().toISOString().replace(/[-:.]/g, '');
+    const uniqueString = uuidv4();
+    const generatedFileName = `${fileName}_${dateTime}_${uniqueString}`;
+    return generatedFileName;
 }
 
-export default FileHandler;
+export function generateFilePath(componentName: string): string {
+    return "/$HOME/Deakin-Detonator-Toolkit/temp/**" + componentName;
+}
