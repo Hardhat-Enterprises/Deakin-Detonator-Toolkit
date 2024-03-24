6beac446afbaaeef5d92d9b7d844bc196f269fdd
zkkok <zhenkangko@gmail.com>
2024-03-24 02:55:35 -0400

    created overlay for pkexec that successfully terminates upon cancellation

--- src/components/OverlayAndCancelButton/OverlayAndCancelButton.tsx
+++ src/components/OverlayAndCancelButton/OverlayAndCancelButton.tsx
@@ -23,3 +23,32 @@ export function LoadingOverlayAndCancelButton(loading: boolean, pid: string) {
         </>
     );
 }
+
+//Overlay to successfully terminate processes for tools using pkexec
+export function LoadingOverlayAndCancelButtonPkexec(
+    loading: boolean,
+    pid: string,
+    onData: (data: string) => void,
+    onTermination: ({ code, signal }: { code: number; signal: number }) => void
+) {
+    // Sends a SIGTERM signal to gracefully terminate the active process passed as an argument
+    const handleCancel = () => {
+        if (pid !== null) {
+            const args = [`-2`, pid];
+            CommandHelper.runCommandWithPkexec("kill", args, onData, onTermination);
+        }
+    };
+
+    return (
+        <>
+            <LoadingOverlay visible={loading} />
+            {loading && (
+                <div>
+                    <Button variant="outline" color="red" style={{ zIndex: 1001 }} onClick={handleCancel}>
+                        Cancel
+                    </Button>
+                </div>
+            )}
+        </>
+    );
+}
