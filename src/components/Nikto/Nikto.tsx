6a31a7f360c5b331dce1915ecf0ac9bd1be7ecb0
SethJ1 <sethj4774@gmail.com>
2024-04-08 13:23:40 +1000

    added user guide

--- src/components/Nikto/Nikto.tsx
+++ src/components/Nikto/Nikto.tsx
@@ -4,8 +4,15 @@ import { useForm } from "@mantine/form";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
+import { UserGuide } from "../UserGuide/UserGuide";
 
 const title = "Nikto";
+const description_userguide =
+    "Nikto is a powerful web server scanner that can perform comprehensive tests against web servers for multiple items.\n" +
+    "How to use this Nikto tool:\n" +
+    "- Provide the target URL or IP address to scan.\n" +
+    "- Start the scan to gather information about potential vulnerabilities and misconfigurations.\n" +
+    "- Review the scan output to identify any security issues.\n";
 
 const Nikto = () => {
     return (null);
