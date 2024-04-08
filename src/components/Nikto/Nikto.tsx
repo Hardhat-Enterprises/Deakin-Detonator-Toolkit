b49fc3b33caf95f97d66785ee08bf76341b08afb
SethJ1 <sethj4774@gmail.com>
2024-04-08 13:17:48 +1000

    import dependencies

--- src/components/Nikto/Nikto.tsx
+++ src/components/Nikto/Nikto.tsx
@@ -1,3 +1,10 @@
+import React, { useState } from 'react';
+import { Button, LoadingOverlay, Stack, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { CommandHelper } from "../../utils/CommandHelper";
+import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
+import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
+
 const title = "Nikto";
 
 const Nikto = () => {
