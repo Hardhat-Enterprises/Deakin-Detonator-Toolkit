8ee727b72e1da96ba9493ce9ce6049d9d9c1550b
zkkok <zhenkangko@gmail.com>
2024-04-15 21:41:31 -0400

    Updated expection handling for exceptions and when pid is null

--- src/components/OverlayAndCancelButton/OverlayAndCancelButton.tsx
+++ src/components/OverlayAndCancelButton/OverlayAndCancelButton.tsx
@@ -1,5 +1,9 @@
 import { LoadingOverlay, Button } from "@mantine/core";
 import { CommandHelper } from "../../utils/CommandHelper";
+import { useCallback ,useState} from "react";
+import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
+
+
 
 export function LoadingOverlayAndCancelButton(loading: boolean, pid: string) {
     // Sends a SIGTERM signal to gracefully terminate the active process passed as an argument
@@ -30,13 +34,24 @@ export function LoadingOverlayAndCancelButtonPkexec(
     pid: string,
     onData: (data: string) => void,
     onTermination: ({ code, signal }: { code: number; signal: number }) => void
+
 ) {
     // Sends a SIGTERM signal to gracefully terminate the active process passed as an argument
     const handleCancel = () => {
-        if (pid !== null) {
-            const args = [`-2`, pid];
-            CommandHelper.runCommandWithPkexec("kill", args, onData, onTermination);
+        try{
+            if (pid !== null) {
+                const args = [`-2`, pid];
+                CommandHelper.runCommandWithPkexec("kill", args, onData, onTermination);
+            } 
+            else{   
+                throw(new Error("Error: Failed to get process ID "))
+            }           
         }
+        catch(e)
+        {
+            throw(e)
+        }
+
     };
 
     return (
