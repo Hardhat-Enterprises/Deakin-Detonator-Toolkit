e95e7401d802078c492e4c21de4a4fe38687727b
bradley <bradley@darkapex.au>
2024-04-15 20:50:48 -0400

    Refactor AircrackNG component to remove unused import and reorder advanced mode.

--- src/components/AircrackNG/AircrackNG.tsx
+++ src/components/AircrackNG/AircrackNG.tsx
@@ -1,4 +1,4 @@
-import { Button, NativeSelect, Select, Stack, TextInput, Switch } from "@mantine/core";
+import { Button, NativeSelect, Stack, TextInput, Switch } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useCallback, useState, useEffect } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
