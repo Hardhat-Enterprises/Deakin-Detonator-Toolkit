c18597c970e12de8314b6b49d6126e3895412591
bradley <bradley@darkapex.au>
2024-04-15 21:26:07 -0400

    Refactor AirbaseNG

--- src/components/AirbaseNG/AirbaseNG.tsx
+++ src/components/AirbaseNG/AirbaseNG.tsx
@@ -1,48 +1,73 @@
 import { Button, Stack, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { UserGuide } from "../UserGuide/UserGuide";
 import { SaveOutputToTextFile } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
+import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
+import InstallationModal from "../InstallationModal/InstallationModal";
+
+// Component constants.
+const title = "Airbase-Ng"; // Contains the title of the component.
+
+// Contains the description of the component.
+const description_userguide =
+    "Airbase-Ng is a component of the Aircrack-Ng suite. Its primary purpose is to create fake access points and perform various " +
+    "attacks on wireless networks.\n\n" +
+    "How to use Airbase-Ng:\n\n" +
+    "Step 1: Type in the name of your fake host. This will be used as the SSID (Service Set Identifier). E.g. Free WiFi.\n" +
+    "Step 2: Specify the WLAN interface to be used.\n" +
+    "Step 3: Click 'Start AP' to begin the process.\n" +
+    "Step 4: View the Output block below to see the results.\n" +
+    "Optionally you may select additional advanced options.";
 
 /**
- * TODO:
- * 1. Refine the user interface for better usability and integrate a mechanism for file selection from the local machine.
- * 2. Introduce an 'Advanced Mode' for users familiar with the nuances of the tool.
- * 3. Gradually expand input options in 'Advanced Mode' with the eventual aim of encompassing all functionalities of `airbase-ng`.
- * 4. Enhance the output display to ensure optimal readability, especially for extensive outputs.
- * 5. Unblock the loading screen during the up time of the Airbase ng and provide real time data update
+ * Represents the form values for the AirbaseNG component.
  */
-
-const title = "Create Fake Access Point with Airbase-ng";
-const description_userguide =
-    "Airbase-ng is a tool to create fake access points.\n\n" +
-    "Step 1: Type in the name of your fake host.\n" +
-    "Step 2: Select your desired channel.\n" +
-    "Step 3: Specify the WLAN interface to be used.\n" +
-    "Step 4: Click 'Start AP' to begin the process.\n" +
-    "Step 5: View the Output block below to see the results.\n\n";
-
 interface FormValuesType {
-    FakeHost: string;
-    Channel: string;
-    Wlan: string;
+    fakeHost: string;
+    channel: string;
+    wlan: string;
 }
 
 const AirbaseNG = () => {
-    const [loading, setLoading] = useState(false);
-    const [output, setOutput] = useState("");
-    const [pid, setPid] = useState("");
+    // Component State Variables.
+    const [loading, setLoading] = useState(false); // State variable to indicate loading state.
+    const [output, setOutput] = useState(""); // State variable to store the output of the command execution.
+    const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
+    const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
+    const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
 
+    // Component Constants.
+    const dependencies = ["aircrack-ng"]; // Contains the dependencies required by the component.
+
+    // Check if the command is available and set the state variables accordingly.
+    useEffect(() => {
+        // Check if the command is available and set the state variables accordingly.
+        checkAllCommandsAvailability(dependencies)
+            .then((isAvailable) => {
+                setIsCommandAvailable(isAvailable); // Set the command availability state
+                setOpened(!isAvailable); // Set the modal state to opened if the command is not available
+                setLoadingModal(false); // Set loading to false after the check is done
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
+                setLoadingModal(false); // Also set loading to false in case of error
+            });
+    }, []);
+
+    // Form Hook to handle form input.
     const form = useForm({
         initialValues: {
-            FakeHost: "",
-            Channel: "",
-            Wlan: "",
+            fakeHost: "",
+            channel: "",
+            wlan: "",
         },
     });
+
     /**
      * handleProcessData: Callback to handle and append new data from the child process to the output.
      * It updates the state by appending the new data received to the existing output.
@@ -56,22 +81,29 @@ const AirbaseNG = () => {
      * handleProcessTermination: Callback to handle the termination of the child process.
      * Once the process termination is handled, it clears the process PID reference and
      * deactivates the loading overlay.
-     * @param {object} param0 - An object containing information about the process termination.
-     * @param {number} param0.code - The exit code of the terminated process.
-     * @param {number} param0.signal - The signal code indicating how the process was terminated.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
      */
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
+            // If the process was successful, display a success message.
             if (code === 0) {
                 handleProcessData("\nProcess completed successfully.");
+
+                // If the process was terminated manually, display a termination message.
             } else if (signal === 15) {
                 handleProcessData("\nProcess was manually terminated.");
+
+                // If the process was terminated with an error, display the exit and signal codes.
             } else {
                 handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
             }
-            // Clear the child process pid reference
+
+            // Clear the child process pid reference. There is no longer a valid process running.
             setPid("");
-            // Cancel the Loading Overlay
+
+            // Cancel the loading overlay. The process has completed.
             setLoading(false);
         },
         [handleProcessData] // Dependency on the handleProcessData callback
@@ -82,21 +114,22 @@ const AirbaseNG = () => {
      * It sets up and triggers the airbase-ng tool with the given parameters.
      * Once the command is executed, the results or errors are displayed in the output.
      *
-     * @param {FormValuesType} values - The form values, containing the fake host name, channel, and WLAN interface.
+     * @param {FormValuesType} values - The form values, containing the fake host name and WLAN interface.
      */
     const onSubmit = async (values: FormValuesType) => {
-        // Activate loading state to indicate ongoing process
+        // Activate loading state to indicate ongoing process.
         setLoading(true);
 
-        // Construct arguments for the aircrack-ng command based on form input
-        const args = ["-e", values.FakeHost, "-c", values.Channel, values.Wlan];
+        // Construct arguments for the aircrack-ng command based on form input.
+        const args = ["-e", values.fakeHost, "-c", values.channel, values.wlan];
 
-        // Execute the aircrack-ng command via helper method and handle its output or potential errors
+        // Execute the aircrack-ng command via helper method and handle its output or potential errors.
         CommandHelper.runCommandWithPkexec("airbase-ng", args, handleProcessData, handleProcessTermination)
             .then(({ output, pid }) => {
-                // Update the UI with the results from the executed command
+                // Update the output with the results of the command execution.
                 setOutput(output);
-                console.log(pid);
+
+                // Store the process ID of the executed command.
                 setPid(pid);
             })
             .catch((error) => {
@@ -114,19 +147,30 @@ const AirbaseNG = () => {
     const clearOutput = useCallback(() => {
         setOutput("");
     }, [setOutput]);
+
     return (
-        <form onSubmit={form.onSubmit(onSubmit)}>
-            {LoadingOverlayAndCancelButton(loading, pid)}
-            <Stack>
-                {UserGuide(title, description_userguide)}
-                <TextInput label={"Name of your fake Host"} required {...form.getInputProps("FakeHost")} />
-                <TextInput label={"Channel of choice"} required {...form.getInputProps("Channel")} />
-                <TextInput label={"Your Wlan"} required {...form.getInputProps("Wlan")} />
-                {SaveOutputToTextFile(output)}
-                <Button type={"submit"}>Start AP</Button>
-                <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
-            </Stack>
-        </form>
+        <>
+            {!loadingModal && (
+                <InstallationModal
+                    isOpen={opened}
+                    setOpened={setOpened}
+                    feature_description={description_userguide}
+                    dependencies={dependencies}
+                ></InstallationModal>
+            )}
+            <form onSubmit={form.onSubmit(onSubmit)}>
+                {LoadingOverlayAndCancelButton(loading, pid)}
+                <Stack>
+                    {UserGuide(title, description_userguide)}
+                    <TextInput label={"Service Set Identifier (SSID)"} required {...form.getInputProps("fakeHost")} />
+                    <TextInput label={"Channel of choice"} required {...form.getInputProps("channel")} />
+                    <TextInput label={"WLAN Interface"} required {...form.getInputProps("wlan")} />
+                    {SaveOutputToTextFile(output)}
+                    <Button type={"submit"}>Start Access Point</Button>
+                    <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+                </Stack>
+            </form>
+        </>
     );
 };
 
