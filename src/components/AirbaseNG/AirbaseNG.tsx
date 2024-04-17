1c8007d4012e1cb034b6d673b74fd2d192833ca3
bradley <bradley@darkapex.au>
2024-04-16 22:01:57 -0400

    Add checkAllCommandsAvailability function and InstallationModal component to AirbaseNG.tsx

--- src/components/AirbaseNG/AirbaseNG.tsx
+++ src/components/AirbaseNG/AirbaseNG.tsx
@@ -1,11 +1,13 @@
 import { Button, Stack, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { RenderComponent } from "../UserGuide/UserGuide";
 import { SaveOutputToTextFile } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
+import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
+import InstallationModal from "../InstallationModal/InstallationModal";
 
 /**
  * Represents the form values for the AirbaseNG component.
