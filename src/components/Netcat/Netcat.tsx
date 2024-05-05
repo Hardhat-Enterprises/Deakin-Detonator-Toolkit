809bf0e09f134f261a1aff014dcab42c7d3ec607
anjojoseph <anjojosephapj@gmailcom>
2024-05-04 21:40:52 -0400

    Merging conflicts step1: pull code from main

--- src/components/Netcat/Netcat.tsx
+++ src/components/Netcat/Netcat.tsx
@@ -1,12 +1,10 @@
-import { Button, NativeSelect, Stack, TextInput, LoadingOverlay } from "@mantine/core";
+import { Button, NativeSelect, Stack, TextInput, Checkbox } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
-import { UserGuide } from "../UserGuide/UserGuide";
-import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
-import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { SaveOutputToTextFile } from "../SaveOutputToFile/SaveOutputToTextFile";
+import { UserGuide } from "../UserGuide/UserGuide";
 
 const title = "Netcat Tool";
 const description_userguide =
@@ -32,22 +30,13 @@ interface FormValuesType {
 }
 
 //Netcat Options
-const netcatOptions = [
-    "Port Scan",
-    "Send File",
-    "Receive File",
-    "Website Port scan",
-    "Interactive terminal/Listen (for chat or reverse shell) [Beta]",
-];
+const netcatOptions = ["Port Scan", "Send File", "Receive File", "Website Port scan"];
 
 //Tool name must be capital or jsx will cry out errors :P
 const NetcatTool = () => {
     var [output, setOutput] = useState("");
     const [selectedScanOption, setSelectedNetcatOption] = useState("");
-    const [pid, setPid] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [allowSave, setAllowSave] = useState(false);
-    const [hasSaved, setHasSaved] = useState(false);
+    const [checkedVerboseMode, setCheckedVerboseMode] = useState(false);
 
     let form = useForm({
         initialValues: {
@@ -59,157 +48,82 @@ const NetcatTool = () => {
         },
     });
 
-    const handleProcessData = useCallback((data: string) => {
-        setOutput((prevOutput) => prevOutput + "\n" + data); // Update output
-    }, []);
-    // Uses the onTermination callback function of runCommandGetPidAndOutput to handle
-    // the termination of that process, resetting state variables, handling the output data,
-    // and informing the user.
-    const handleProcessTermination = useCallback(
-        ({ code, signal }: { code: number; signal: number }) => {
-            if (code === 0) {
-                handleProcessData("\nProcess completed successfully.");
-            } else if (signal === 15) {
-                handleProcessData("\nProcess was manually terminated.");
-            } else {
-                handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
-            }
-            // Clear the child process pid reference
-            setPid("");
-            // Cancel the Loading Overlay
-            setLoading(false);
-
-            // Allow Saving as the output is finalised
-            setAllowSave(true);
-            setHasSaved(false);
-        },
-        [handleProcessData]
-    );
-
-    // Sends a SIGTERM signal to gracefully terminate the process
-    const handleCancel = () => {
-        if (pid !== null) {
-            const args = [`-15`, pid];
-            CommandHelper.runCommand("kill", args);
-        }
-    };
-
-    // Actions taken after saving the output
-    const handleSaveComplete = () => {
-        // Indicating that the file has saved which is passed
-        // back into SaveOutputToTextFile to inform the user
-        setHasSaved(true);
-        setAllowSave(false);
-    };
-
     const onSubmit = async (values: FormValuesType) => {
-        // Disallow saving until the tool's execution is complete
-        setAllowSave(false);
-        setLoading(true);
         //Starts off with the IP address after netcat
         //Ex: nc <ip address>
         let args = [``];
+
+        //If verbose mode is checked, v flag is added to args
+        const verboseFlag = checkedVerboseMode ? "v" : "";
 
         //Switch case
         switch (values.netcatOptions) {
             case "Port Scan": //nc syntax: nc -zv <ip address/hostname> <port range>
                 //addition of -n will not perform any dns or name lookups.
-                args = [`-zvn`];
+
+                args = [`-z${verboseFlag}n`];
                 args.push(`${values.ipAddress}`);
-                args.push(`${values.portNumber}`);
 
-                try {
-                    let result = await CommandHelper.runCommandGetPidAndOutput(
-                        "nc",
-                        args,
-                        handleProcessData,
-                        handleProcessTermination
-                    );
-                    setPid(result.pid);
-                    setOutput(result.output);
-                } catch (e: any) {
-                    setOutput(e.message);
+                if (values.portNumber.includes("-")) {
+                    //checks for port range specifed by the inclusion of "-"
+                    const [portStart, portEnd] = values.portNumber.split("-").map(Number); //Splits range by "-", assigns two consts with the split port numbers
+
+                    for (let currentPort = portStart; currentPort <= portEnd; currentPort++) {
+                        //Iterates through every port from start to end
+                        try {
+                            let output = await CommandHelper.runCommand("nc", [...args, String(currentPort)]);
+                            setOutput(output);
+                        } catch (e: any) {
+                            setOutput(e);
+                        }
+                    }
+                } else {
+                    //else port number has been inputted
+                    args.push(`${values.portNumber}`);
+
+                    try {
+                        let output = await CommandHelper.runCommand("nc", args);
+                        setOutput(output);
+                    } catch (e: any) {
+                        setOutput(e);
+                    }
                 }
 
                 break;
 
-            case "Send File": //Sends file from attacker to victim, syntax: nc -w 15 <Dest IP address> <Port number> < <FileName>
-                args = [`-w 15`]; //I set the timeout on 15 by default, you can remove it if you want
-                args.push(`${values.ipAddress} ${values.portNumber} < ${values.filePath}`);
-
+            case "Send File": //Sends file from attacker to victim, syntax: nc -v -w <timeout seconds> <IP address> <port number> < <file path>
+                //File to send can be located anywhere, as long as file path is correctly specified
                 try {
-                    let result = await CommandHelper.runCommandGetPidAndOutput(
-                        "nc",
-                        args,
-                        handleProcessData,
-                        handleProcessTermination
-                    );
-                    setPid(result.pid);
-                    setOutput(result.output);
+                    let command = `nc -${verboseFlag} -w 10 ${values.ipAddress} ${values.portNumber} < ${values.filePath}`;
+                    let output = await CommandHelper.runCommand("bash", ["-c", command]); //when using '<', command needs to be run via bash shell to recognise that '<' is an input direction
+                    setOutput(output);
                 } catch (e: any) {
-                    setOutput(e.message);
+                    setOutput(e);
                 }
-
                 break;
 
-            case "Receive File": //Receives file from victim to attacker, syntax: nc -l <port number> > filename.file
-                args = [`-lp`];
-                args.push(`${values.portNumber} > ${values.filePath}`);
-
+            case "Receive File": //Receives file from victim to attacker, syntax: nc -lvp <port number> > <file path and file name>
+                //Files can be recieved in any directory
                 try {
-                    let result = await CommandHelper.runCommandGetPidAndOutput(
-                        "nc",
-                        args,
-                        handleProcessData,
-                        handleProcessTermination
-                    );
-                    setPid(result.pid);
-                    setOutput(result.output);
+                    let command = `nc -l${verboseFlag}p ${values.portNumber} > ${values.filePath}`;
+                    let output = await CommandHelper.runCommand("bash", ["-c", command]);
+                    setOutput(output);
                 } catch (e: any) {
-                    setOutput(e.message);
+                    setOutput(e);
                 }
 
                 break;
 
             case "Website Port scan": //Scans a website for ports, syntax: nc -zv <hostname> <port range>
-                args = [`-zv`]; //PLease only use website portscan on a website/Domain that you own
+                args = [`-z${verboseFlag}`]; //PLease only use website portscan on a website/Domain that you own
                 args.push(`${values.websiteUrl}`);
                 args.push(`${values.portNumber}`);
 
                 try {
-                    let result = await CommandHelper.runCommandGetPidAndOutput(
-                        "nc",
-                        args,
-                        handleProcessData,
-                        handleProcessTermination
-                    );
-                    setPid(result.pid);
-                    setOutput(result.output);
+                    let output = await CommandHelper.runCommand("nc", args);
+                    setOutput(output);
                 } catch (e: any) {
-                    setOutput(e.message);
-                }
-
-                break;
-
-            case "Interactive terminal/Listen (for chat or reverse shell) [Beta]":
-                //Beta stage
-                //The script executes a separate qterminal session that runs netcat listen command
-                //The script runs like the normal netcat listen where the user can communicate between the attacker (kali) and the victim (VM2)
-                //Not sure how to add output to the tool
-                args = [`/usr/share/ddt/Bash-Scripts/netcatTerminal.sh`];
-                args.push(`${values.portNumber}`);
-
-                try {
-                    let result = await CommandHelper.runCommandGetPidAndOutput(
-                        "nc",
-                        args,
-                        handleProcessData,
-                        handleProcessTermination
-                    );
-                    setPid(result.pid);
-                    setOutput(result.output);
-                } catch (e: any) {
-                    setOutput(e.message);
+                    setOutput(e);
                 }
 
                 break;
@@ -218,21 +132,18 @@ const NetcatTool = () => {
 
     const clearOutput = useCallback(() => {
         setOutput("");
-        setHasSaved(false);
-        setAllowSave(false);
     }, [setOutput]);
 
     //<ConsoleWrapper output={output} clearOutputCallback={clearOutput} /> prints the terminal on the tool
     return (
         <form onSubmit={form.onSubmit((values) => onSubmit({ ...values, netcatOptions: selectedScanOption }))}>
-            {LoadingOverlayAndCancelButton(loading, pid)}
-            <LoadingOverlay visible={loading} />
             <Stack>
                 {UserGuide(title, description_userguide)}
-                <TextInput label={"IP address"} {...form.getInputProps("ipAddress")} />
-                <TextInput label={"Port number/Port range"} required {...form.getInputProps("portNumber")} />
-                <TextInput label={"File path"} {...form.getInputProps("filePath")} />
-                <TextInput label={"Domain name"} {...form.getInputProps("websiteUrl")} />
+                <Checkbox
+                    label={"Verbose Mode"}
+                    checked={checkedVerboseMode}
+                    onChange={(e) => setCheckedVerboseMode(e.currentTarget.checked)}
+                />
                 <NativeSelect
                     value={selectedScanOption}
                     onChange={(e) => setSelectedNetcatOption(e.target.value)}
@@ -242,9 +153,33 @@ const NetcatTool = () => {
                     placeholder={"Pick a scan option"}
                     description={"Type of scan to perform"}
                 />
-
-                {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                {selectedScanOption === "Port Scan" && (
+                    <>
+                        <TextInput label={"IP address"} {...form.getInputProps("ipAddress")} />
+                        <TextInput label={"Port number/Port range"} required {...form.getInputProps("portNumber")} />
+                    </>
+                )}
+                {selectedScanOption === "Send File" && (
+                    <>
+                        <TextInput label={"IP address"} {...form.getInputProps("ipAddress")} />
+                        <TextInput label={"Port number/Port range"} required {...form.getInputProps("portNumber")} />
+                        <TextInput label={"File path"} {...form.getInputProps("filePath")} />
+                    </>
+                )}
+                {selectedScanOption === "Receive File" && (
+                    <>
+                        <TextInput label={"Port number/Port range"} required {...form.getInputProps("portNumber")} />
+                        <TextInput label={"File path"} {...form.getInputProps("filePath")} />
+                    </>
+                )}
+                {selectedScanOption === "Website Port Scan" && (
+                    <>
+                        <TextInput label={"Port number/Port range"} required {...form.getInputProps("portNumber")} />
+                        <TextInput label={"Domain name"} {...form.getInputProps("websiteUrl")} />
+                    </>
+                )}
                 <Button type={"submit"}>start netcat</Button>
+                {SaveOutputToTextFile(output)}
                 <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
             </Stack>
         </form>
