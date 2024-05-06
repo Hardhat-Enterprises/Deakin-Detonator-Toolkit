a0f1a1296353b9ab3bfd5304813bb77e33806b47
SethJ1 <sethj4774@gmail.com>
2024-05-06 14:15:35 +1000

    fixed changes

--- src/components/Nikto/Nikto.tsx
+++ src/components/Nikto/Nikto.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import { Button, Checkbox, LoadingOverlay, Stack, TextInput } from "@mantine/core";
+import { useState, useEffect, useCallback } from "react";
+import { Button, Checkbox, Stack, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { RenderComponent } from "../UserGuide/UserGuide";
+import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
+import InstallationModal from "../InstallationModal/InstallationModal";
+import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
 
 /**
  * Represents the form values for the Nikto component.
@@ -25,6 +28,10 @@ const Nikto = () => {
     const [allowSave, setAllowSave] = useState(false); // State variable to allow saving of output
     const [hasSaved, setHasSaved] = useState(false); // State variable to indicate if output has been saved
     const [sslScan, setSslScan] = useState(false); // State variable for SSL scanning
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
+    const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
+    const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
+    const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
 
     // Component Constants
     const title = "Nikto";
@@ -36,6 +43,7 @@ const Nikto = () => {
         "Step 3: Review the scan output to identify any security issues.\n";
     const sourceLink = "https://github.com/sullo/nikto"; // Link to the source code
     const tutorial = "https://github.com/sullo/nikto/wiki"; // Link to the official documentation/tutorial
+    const dependencies = ["nikto"]; // Contains the dependencies required by the component.
 
     // Form hook to handle form input
     let form = useForm({
@@ -45,32 +53,88 @@ const Nikto = () => {
         },
     });
 
+    // Check if the command is available and set the state variables accordingly.
+    useEffect(() => {
+        // Check if the command is available and set the state variables accordingly.
+        checkAllCommandsAvailability(dependencies)
+            .then((isAvailable) => {
+                setIsCommandAvailable(isAvailable); // Set the command availability state
+                setOpened(!isAvailable); // Set the modal state to opened if the command is not available
+                setLoadingModal(false); // Set loading to false after the check is done
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
+                setLoadingModal(false); // Also set loading to false in case of error
+            });
+    }, []);
+
+    /**
+     * handleProcessData: Callback to handle and append new data from the child process to the output.
+     * It updates the state by appending the new data received to the existing output.
+     * @param {string} data - The data received from the child process.
+     */
+    const handleProcessData = useCallback((data: string) => {
+        setOutput((prevOutput) => prevOutput + "\n" + data); // Append new data to the previous output.
+    }, []);
+
+    /**
+     * handleProcessTermination: Callback to handle the termination of the child process.
+     * Once the process termination is handled, it clears the process PID reference and
+     * deactivates the loading overlay.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
+     */
+    const handleProcessTermination = useCallback(
+        ({ code, signal }: { code: number; signal: number }) => {
+            // If the process was successful, display a success message.
+            if (code === 0) {
+                handleProcessData("\nProcess completed successfully.");
+
+                // If the process was terminated manually, display a termination message.
+            } else if (signal === 15) {
+                handleProcessData("\nProcess was manually terminated.");
+
+                // If the process was terminated with an error, display the exit and signal codes.
+            } else {
+                handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
+            }
+
+            // Clear the child process pid reference. There is no longer a valid process running.
+            setPid("");
+
+            // Cancel the loading overlay. The process has completed.
+            setLoading(false);
+        },
+        [handleProcessData] // Dependency on the handleProcessData callback
+    );
+
     /**
      * Handles form submission for the Nikto component.
      * @param {FormValuesType} values - The form values containing the target URL and SSL scan option.
      */
     const onSubmit = async (values: FormValuesType) => {
-        // Set loading state to true and disallow output saving
+        // Activate loading state to indicate ongoing process
         setLoading(true);
-        setAllowSave(false);
 
-        // Execute Nikto command with provided target URL
-        try {
-            // Adjusting the command arguments based on checkbox state for SSL scanning
-            const args = ["-h", values.targetURL];
-            if (values.sslScan) {
-                args.push("-ssl"); // Add -ssl option for SSL scanning
-            }
-            const commandOutput = await CommandHelper.runCommand("nikto", args);
-            // Update output state with command output
-            setOutput(commandOutput);
-        } catch (error: any) {
-            setOutput(`Error: ${error.message}`);
-        } finally {
-            // Set loading state to false and allow output saving
-            setLoading(false);
-            setAllowSave(true);
+        // Construct arguments for the Nikto command based on form input
+        const args = ["-h", values.targetURL];
+        if (values.sslScan) {
+            args.push("-ssl"); // Add -ssl option for SSL scanning
         }
+
+        // Execute the Nikto command via helper method and handle its output or potential errors
+        CommandHelper.runCommandWithPkexec("nikto", args, handleProcessData, handleProcessTermination)
+            .then(() => {
+                // Deactivate loading state
+                setLoading(false);
+            })
+            .catch((error) => {
+                // Display any errors encountered during command execution
+                setOutput(`Error: ${error.message}`);
+                // Deactivate loading state
+                setLoading(false);
+            });
     };
 
     /**
@@ -99,9 +163,17 @@ const Nikto = () => {
             tutorial={tutorial}
             sourceLink={sourceLink}
         >
+            {!loadingModal && (
+                <InstallationModal
+                    isOpen={opened}
+                    setOpened={setOpened}
+                    feature_description={description}
+                    dependencies={dependencies}
+                />
+            )}
             <form onSubmit={form.onSubmit(onSubmit)}>
                 <Stack>
-                    {loading && <LoadingOverlay visible={true} />}
+                    {LoadingOverlayAndCancelButton(loading, pid)}
                     <TextInput label="Target URL" required {...form.getInputProps("targetURL")} />
                     <Checkbox
                         label="SSL Scan"
