2e699d4e1511236ac644bcb6b0c048f3d54833d5
CAfitech <aafiyairfan2113@gmail.com>
2024-05-07 23:53:46 -0400

    I am modifying to re-implement the old functonailty of this component, final change.

--- src/components/AirbaseNG/AirbaseNG.tsx
+++ src/components/AirbaseNG/AirbaseNG.tsx
@@ -4,7 +4,6 @@ import { useCallback, useState, useEffect } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { RenderComponent } from "../UserGuide/UserGuide";
-import { UserGuide } from "../UserGuide/UserGuide";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile"; //v2
 import { LoadingOverlayAndCancelButtonPkexec } from "../OverlayAndCancelButton/OverlayAndCancelButton";
 import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
@@ -24,6 +23,7 @@ interface FormValuesType {
  * @returns The AirbaseNG component.
  */
 const AirbaseNG = () => {
+    // Component State Variables.
     const [loading, setLoading] = useState(false); // State variable to indicate loading state.
     const [output, setOutput] = useState(""); // State variable to store the output of the command execution.
     const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
@@ -33,15 +33,20 @@ const AirbaseNG = () => {
     const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
     const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
 
-    const title = "Create Fake Access Point with Airbase-ng";
-    const description_userguide =
-        "Airbase-ng is a tool to create fake access points.\n\n" +
+    // Component Constants.
+    const title = "Airbase-ng"; // Title of the component.
+    const description = "Airbase-ng is a tool to create fake access points."; // Description of the component
+    const steps =
         "Step 1: Type in the name of your fake host.\n" +
         "Step 2: Select your desired channel.\n" +
         "Step 3: Specify the WLAN interface to be used.\n" +
         "Step 4: Click 'Start AP' to begin the process.\n" +
-        "Step 5: View the Output block below to see the results.\n\n";
+        "Step 5: View the output block to see the results. ";
+    const sourceLink = ""; // Link to the source code (or Kali Tools).
+    const tutorial = ""; // Link to the official documentation/tutorial.
+    const dependencies = ["aircrack-ng"]; // Contains the dependencies required by the component.
 
+    // Form hook to handle form input.
     const form = useForm({
         initialValues: {
             FakeHost: "",
@@ -49,6 +54,22 @@ const AirbaseNG = () => {
             Wlan: "",
         },
     });
+
+    // Check if the command is available and set the state variables accordingly.
+    useEffect(() => {
+        // Check if the command is available and set the state variables accordingly.
+        checkAllCommandsAvailability(dependencies)
+            .then((isAvailable) => {
+                setIsCommandAvailable(isAvailable); // Set the command availability state
+                setOpened(!isAvailable); // Set the modal state to opened if the command is not available
+                setLoadingModal(false); // Set loading to false after the check is done
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
+                setLoadingModal(false); // Also set loading to false in case of error
+            });
+    }, []);
+
     /**
      * handleProcessData: Callback to handle and append new data from the child process to the output.
      * It updates the state by appending the new data received to the existing output.
@@ -62,9 +83,9 @@ const AirbaseNG = () => {
      * handleProcessTermination: Callback to handle the termination of the child process.
      * Once the process termination is handled, it clears the process PID reference and
      * deactivates the loading overlay.
-     * @param {object} param0 - An object containing information about the process termination.
-     * @param {number} param0.code - The exit code of the terminated process.
-     * @param {number} param0.signal - The signal code indicating how the process was terminated.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
      */
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
@@ -109,6 +130,7 @@ const AirbaseNG = () => {
             .then(({ output, pid }) => {
                 // Update the UI with the results from the executed command
                 setOutput(output);
+                setAllowSave(true);
                 console.log(pid);
                 setPid(pid);
             })
@@ -122,8 +144,7 @@ const AirbaseNG = () => {
     };
 
     /**
-     * clearOutput: Callback function to clear the console output.
-     * It resets the state variable holding the output, thereby clearing the display.
+     * Clears the output state.
      */
     const clearOutput = useCallback(() => {
         setOutput("");
@@ -139,18 +160,33 @@ const AirbaseNG = () => {
     };
 
     return (
-        <form onSubmit={form.onSubmit(onSubmit)}>
-            {LoadingOverlayAndCancelButtonPkexec(loading, pid, handleProcessData, handleProcessTermination)}
-            <Stack>
-                {UserGuide(title, description_userguide)}
-                <TextInput label={"Name of your fake Host"} required {...form.getInputProps("FakeHost")} />
-                <TextInput label={"Channel of choice"} required {...form.getInputProps("Channel")} />
-                <TextInput label={"Your Wlan"} required {...form.getInputProps("Wlan")} />
-                {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
-                <Button type={"submit"}>Start AP</Button>
-                <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
-            </Stack>
-        </form>
+        <RenderComponent
+            title={title}
+            description={description}
+            steps={steps}
+            tutorial={tutorial}
+            sourceLink={sourceLink}
+        >
+            {!loadingModal && (
+                <InstallationModal
+                    isOpen={opened}
+                    setOpened={setOpened}
+                    feature_description={description}
+                    dependencies={dependencies}
+                ></InstallationModal>
+            )}
+            <form onSubmit={form.onSubmit(onSubmit)}>
+                {LoadingOverlayAndCancelButtonPkexec(loading, pid, handleProcessData, handleProcessTermination)}
+                <Stack>
+                    <TextInput label={"Name of your fake Host"} required {...form.getInputProps("FakeHost")} />
+                    <TextInput label={"Channel of choice"} required {...form.getInputProps("Channel")} />
+                    <TextInput label={"Your Wlan"} required {...form.getInputProps("Wlan")} />
+                    {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                    <Button type={"submit"}>Start AP</Button>
+                    <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+                </Stack>
+            </form>
+        </RenderComponent>
     );
 };
 
