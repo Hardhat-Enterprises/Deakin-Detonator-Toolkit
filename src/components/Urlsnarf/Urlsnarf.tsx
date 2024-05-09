c0b4588c5de6b94d7f992c30b13b36dc7eae3a1f
slpysiren <slpyneris@gmail.com>
2024-05-09 13:03:36 +1000

    Remove deprecated UserGuide and implement updated version

--- src/components/Urlsnarf/Urlsnarf.tsx
+++ src/components/Urlsnarf/Urlsnarf.tsx
@@ -3,25 +3,9 @@ import { useForm } from "@mantine/form";
 import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
-import { UserGuide } from "../UserGuide/UserGuide";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
-
-const title = "Urlsnarf";
-const description_userguide =
-    "Urlsnarf is a network traffic sniffing tool that works to output all URL's that are requested from HTTP " +
-    "traffic in the from of CLF (Common Log Format) that is very commonly used within web servers. The tool " +
-    "in the DDT provides two listener settings being through an interface or packet capture file.\n\nInformation " +
-    "on the tool can be found at: https://linux.die.net/man/8/urlsnarf\n\n" +
-    "Using the tool:\n" +
-    "Step 1: Select the Listener settings.\n" +
-    "       Eg: Interface\n\n" +
-    "Step 2: Input the Interface.\n" +
-    "       Eg: eth0\n\n" +
-    "Step 3: Enter any Exclusion details within the sniff.\n" +
-    "       Eg: POST (every packet besides POST will be shown)\n\n" +
-    "Step 4: Click Sniff to commence Urlsnarf's operation.\n\n" +
-    "Step 5: View the Output block below to view the results of the tools execution.";
+import { RenderComponent } from "../UserGuide/UserGuide";
 
 interface FormValuesType {
     listenerInputType: string;
@@ -38,6 +22,19 @@ const Urlsnarf = () => {
     const [pid, setPid] = useState("");
     const [allowSave, setAllowSave] = useState(false);
     const [hasSaved, setHasSaved] = useState(false);
+
+    const title = "Urlsnarf";
+    const description =
+        "Urlsnarf outputs all requested URLs sniffed from HTTP traffic in Common Log Format, used by almost all web servers.";
+    const steps =
+        "Step 1: Select the Listener settings.\n" +
+        "Step 2: Input the Interface.\n" +
+        "Step 3: Enter any Exclusion details within the sniff.\n" +
+        "Step 4: Click Sniff to start Urlsnarf.\n" +
+        "Step 5: View the Output block to see the results.";
+    const sourceLink = "https://linux.die.net/man/8/urlsnarf";
+    const tutorial = "";
+    const dependencies = ["dsniff"];
 
     let form = useForm({
         initialValues: {
@@ -123,53 +120,60 @@ const Urlsnarf = () => {
     const isListenerFile = selectedListenerInput === "Packet capture file";
 
     return (
-        <form
-            onSubmit={form.onSubmit((values) =>
-                onSubmit({
-                    ...values,
-                    listenerInputType: selectedListenerInput,
-                })
-            )}
+        <RenderComponent
+            title={title}
+            description={description}
+            steps={steps}
+            tutorial={tutorial}
+            sourceLink={sourceLink}
         >
-            {LoadingOverlayAndCancelButton(loading, pid)}
-            <Stack>
-                {UserGuide(title, description_userguide)}
-                <NativeSelect
-                    value={selectedListenerInput}
-                    onChange={(e) => setSelectedListenerInput(e.target.value)}
-                    label={"listener settings"}
-                    data={listeners}
-                    required
-                    placeholder={"Interface or PCAP file"}
-                />
-                {isListenerInterface && (
-                    <TextInput
-                        {...form.getInputProps("listenerArgs")}
-                        label={"Interface"}
-                        placeholder={"eg: eth0"}
-                        required
-                    />
+            <form
+                onSubmit={form.onSubmit((values) =>
+                    onSubmit({
+                        ...values,
+                        listenerInputType: selectedListenerInput,
+                    })
                 )}
-                {isListenerFile && (
-                    <TextInput
-                        {...form.getInputProps("listenerArgs")}
-                        label={"File path"}
-                        placeholder={"eg: /home/kali/Desktop/pcap.pcapng"}
+            >
+                {LoadingOverlayAndCancelButton(loading, pid)}
+                <Stack>
+                    <NativeSelect
+                        value={selectedListenerInput}
+                        onChange={(e) => setSelectedListenerInput(e.target.value)}
+                        label={"listener settings"}
+                        data={listeners}
                         required
+                        placeholder={"Interface or PCAP file"}
                     />
-                )}
-                <TextInput
-                    {...form.getInputProps("versusMode")}
-                    label={"Exclusion details"}
-                    placeholder={"eg: POST ; show every packet that excludes POST"}
-                />
-                <Button type={"submit"} color="cyan">
-                    Sniff
-                </Button>
-                {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
-                <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
-            </Stack>
-        </form>
+                    {isListenerInterface && (
+                        <TextInput
+                            {...form.getInputProps("listenerArgs")}
+                            label={"Interface"}
+                            placeholder={"eg: eth0"}
+                            required
+                        />
+                    )}
+                    {isListenerFile && (
+                        <TextInput
+                            {...form.getInputProps("listenerArgs")}
+                            label={"File path"}
+                            placeholder={"eg: /home/kali/Desktop/pcap.pcapng"}
+                            required
+                        />
+                    )}
+                    <TextInput
+                        {...form.getInputProps("versusMode")}
+                        label={"Exclusion details"}
+                        placeholder={"eg: POST ; show every packet that excludes POST"}
+                    />
+                    <Button type={"submit"} color="cyan">
+                        Sniff
+                    </Button>
+                    {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                    <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+                </Stack>
+            </form>
+        </RenderComponent>
     );
 };
 
