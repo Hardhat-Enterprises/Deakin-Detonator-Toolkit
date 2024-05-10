c88e9a8dd04510dc7cb628250abe7462a99060c3
zkkok <zhenkangko@gmail.com>
2024-05-10 02:45:24 -0400

    Added docstrings and comments

--- src/components/metagoofil/metagoofil.tsx
+++ src/components/metagoofil/metagoofil.tsx
@@ -3,12 +3,15 @@ import { useForm } from "@mantine/form";
 import { useCallback, useState, useEffect } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
-import { SaveOutputToTextFile, SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
+import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
 import { RenderComponent } from "../UserGuide/UserGuide";
 import InstallationModal from "../InstallationModal/InstallationModal";
 import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
 
+/**
+ * Represents the form values for the Metagoofil component.
+ */
 interface FormValuesType {
     webname: string;
     searchmax: string;
@@ -17,32 +20,39 @@ interface FormValuesType {
     filepath: string;
 }
 
+/**
+ * The Metagoofil component.
+ * @returns The Metagoofil component.
+ */
 function Metagoofil() {
-    const [loading, setLoading] = useState(false);
-    const [output, setOutput] = useState("");
-    const [pid, setPid] = useState("");
-    const [customconfig, setCustomconfig] = useState(false);
-    const [downloadconfig, setDownloadConfig] = useState(false);
-    const [isCommandAvailable, setIsCommandAvailable] = useState(false);
-    const [loadingModal, setLoadingModal] = useState(true);
-    const [opened, setOpened] = useState(!isCommandAvailable);
-    const [allowSave, setAllowSave] = useState(false); 
-    const [hasSaved, setHasSaved] = useState(false); 
+    //Component State Variables.
+    const [loading, setLoading] = useState(false); // State variable to indicate loading state.
+    const [output, setOutput] = useState(""); // State variable to store the output of the command execution.
+    const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
+    const [customconfig, setCustomconfig] = useState(false); //State variable to indicate if custom configuration is enabled
+    const [downloadconfig, setDownloadConfig] = useState(false); //State variable to indicate if download configuration is enabled
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
+    const [loadingModal, setLoadingModal] = useState(true); // State variable that indicates if the modal is opened.
+    const [opened, setOpened] = useState(!isCommandAvailable); // State variable to indicate loading state of the modal.
+    const [allowSave, setAllowSave] = useState(false); // State variable to indicate if saving is allowed
+    const [hasSaved, setHasSaved] = useState(false); // State variable to indicate if the output has been saved
 
-    const title = "metagoofil";
+    // Component Constants.
+    const title = "metagoofil"; // Title of the component.
     const description =
-        "Metagoofil is an information gathering tool designed for extracting metadata of public documents (pdf,doc,xls,ppt,docx,pptx,xlsx) belonging to a target company.";
+        "Metagoofil is an information gathering tool designed for extracting metadata of public documents (pdf,doc,xls,ppt,docx,pptx,xlsx) belonging to a target company."; // Description of the component.
     const steps =
         "Step 1: Enter a website URL for the tool to search.\n" +
         "Step 2: Enter the desired number of results.\n" +
         "Step 3: Enter the limit for the number of files to be downloaded\n" +
         "Step 4: Enter the file type name to be extracted.\n" +
         "Step 5: Click scan to commence the Metagoofil operation.\n" +
-        "Step 6: View the Output block below to view the results of the tool's execution.";
-    const sourceLink = "https://www.kali.org/tools/metagoofil/";
-    const tutorial = "";
-    const dependencies = ["metagoofil"];
+        "Step 6: View the Output block below to view the results of the tool's execution."; //Steps to run the component
+    const sourceLink = "https://www.kali.org/tools/metagoofil/"; // Link to the source code (or Kali Tools).
+    const tutorial = ""; // Link to the official documentation/tutorial.
+    const dependencies = ["metagoofil"]; // Contains the dependencies required by the component.
 
+    // Form hook to handle form input.
     let form = useForm({
         initialValues: {
             webname: "",
@@ -71,79 +81,103 @@ function Metagoofil() {
     /**
      * handleProcessData: Callback to handle and append new data from the child process to the output.
      * It updates the state by appending the new data received to the existing output.
-     *
      * @param {string} data - The data received from the child process.
      */
     const handleProcessData = useCallback((data: string) => {
         setOutput((prevOutput) => prevOutput + "\n" + data); // Append new data to the previous output.
     }, []);
+
     /**
      * handleProcessTermination: Callback to handle the termination of the child process.
      * Once the process termination is handled, it clears the process PID reference and
      * deactivates the loading overlay.
-     * @param {object} param0 - An object containing information about the process termination.
-     * @param {number} param0.code - The exit code of the terminated process.
-     * @param {number} param0.signal - The signal code indicating how the process was terminated.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
      */
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
+            // If the process was successful, display a success message.
             if (code === 0) {
                 handleProcessData("\nProcess completed successfully.");
+
+                // If the process was terminated manually, display a termination message.
             } else if (signal === 15) {
                 handleProcessData("\nProcess was manually terminated.");
+
+                // If the process was terminated with an error, display the exit and signal codes.
             } else {
                 handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
             }
-            // Clear the child process pid reference
+
+            // Clear the child process pid reference. There is no longer a valid process running.
             setPid("");
-            // Cancel the Loading Overlay
+
+            // Cancel the loading overlay. The process has completed.
             setLoading(false);
 
             // Allow Saving as the output is finalised
             setAllowSave(true);
             setHasSaved(false);
         },
-        [handleProcessData] // Dependency on the handleProcessData callback
+        [handleProcessData]
     );
 
+    /**
+     * handleSaveComplete: handle state changes when saves are completed
+     * Once the output is saved, prevent duplicate saves
+     */
     const handleSaveComplete = () => {
         //Disallow saving once the output is saved
         setHasSaved(true);
         setAllowSave(false);
     };
 
+    /**
+     * onSubmit: Asynchronous handler for the form submission event.
+     * It sets up and triggers the goldeneye tool with the given parameters.
+     * Once the command is executed, the results or errors are displayed in the output.
+     *
+     * @param {FormValuesType} values - The form values, containing the url, userAgent, worker, sockets, method, sslCheck
+     */
     const onSubmit = async (values: FormValuesType) => {
+        // Activate loading state to indicate ongoing process
         setLoading(true);
 
+        // Construct arguments for the Metagoofil command based on form input
         const args = [`-d`, `${values.webname}`, `-t`, `${values.filetype}`];
-
-        //number of searches made
         values.searchmax ? args.push(`-l`, `${values.searchmax}`) : undefined;
-
-        //number of files wanted to be downloaded
         values.filelimit ? args.push(`-n`, `${values.filelimit}`) : undefined;
-
-        //filepath of where downloaded files are to be stored
         values.filepath ? args.push(`-o`, `${values.filepath}`, `-w`) : undefined;
 
         try {
+            // Execute the Metagoofil command via helper method and handle its output or potential errors
             const result = await CommandHelper.runCommandGetPidAndOutput(
                 "metagoofil",
                 args,
                 handleProcessData,
                 handleProcessTermination
             );
+
+            // Update the UI with the results from the executed command
             setPid(result.pid);
             setOutput(result.output);
+            console.log(pid);
         } catch (e: any) {
+            // Display any errors encountered during command execution
             setOutput(e.message);
+            // Deactivate loading state
+            setLoading(false);
         }
     };
 
+    /**
+     * Clears the output state.
+     */
     const clearOutput = useCallback(() => {
         setOutput("");
 
-
+        //Disallow saving when output is cleared
         setHasSaved(false);
         setAllowSave(false);
     }, [setOutput]);
@@ -200,7 +234,7 @@ function Metagoofil() {
                     )}
 
                     <Button type={"submit"}>Scan</Button>
-                    {SaveOutputToTextFile_v2(output,allowSave,hasSaved,handleSaveComplete)}
+                    {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
                     <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
                 </Stack>
             </form>
