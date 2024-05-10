b0d7f34b47c9e1267bde40741076bd6aacc40595
jonschlack <jschlack@deakin.edu.au>
2024-05-10 03:05:27 -0400

    Ran Prettier

--- src/components/Shodan/Shodan-API-Tool.tsx
+++ src/components/Shodan/Shodan-API-Tool.tsx
@@ -9,7 +9,9 @@ import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/Overlay
 import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
 import InstallationModal from "../InstallationModal/InstallationModal";
 
-
+/**
+ * Represents the form values for the Shodan API Tool component.
+ */
 interface FormValuesType {
     hostIP: string;
     shodanKey: string;
@@ -31,18 +33,19 @@ export function ShodanAPITool() {
 
     // Component Constants.
     const title = "Shodan API Tool"; // Title of the component.
-    const description = "The Shodan API is a powerful tool that allows external network scans to be performed with use of a valid API key"; // Description of the component.
-    const steps = 
-    "How to use Shodan API:\n" +
-    "Step 1: Install shodkey.py to /usr/share/ddt by running install_exploits.sh script or manually transfer shodkey.py to /usr/share/ddt/\n"+
-    "Step 2: Enter a Valid API Key\n" +
-    "Step 3: Enter a Host IP: E.g. 127.0.0.1\n" +
-    "Step 4: Click Scan button to commence the Shodan API operation. Or click Cancel Scan to terminate scan\n" +
-    "Step 5: View the Output block below to view the results of the tool's execution.\n" +
-    "Step 6: Optional: to save scan results enter filename and click on the save output to file button";
+    const description =
+        "The Shodan API is a powerful tool that allows external network scans to be performed with use of a valid API key"; // Description of the component.
+    const steps =
+        "How to use Shodan API:\n" +
+        "Step 1: Install shodkey.py to /usr/share/ddt by running install_exploits.sh script or manually transfer shodkey.py to /usr/share/ddt/\n" +
+        "Step 2: Enter a Valid API Key\n" +
+        "Step 3: Enter a Host IP: E.g. 127.0.0.1\n" +
+        "Step 4: Click Scan button to commence the Shodan API operation. Or click Cancel Scan to terminate scan\n" +
+        "Step 5: View the Output block below to view the results of the tool's execution.\n" +
+        "Step 6: Optional: to save scan results enter filename and click on the save output to file button";
     const sourceLink = "https://developer.shodan.io/api"; // Link to the source code.
-    const tutorial = "";  // Link to the official documentation/tutorial.
-    const dependencies = ["python3"] // Contains the dependencies required by the component.
+    const tutorial = ""; // Link to the official documentation/tutorial.
+    const dependencies = ["python3"]; // Contains the dependencies required by the component.
 
     // Form Hook to handle form input.
     let form = useForm({
@@ -55,23 +58,22 @@ export function ShodanAPITool() {
     useEffect(() => {
         // Check if the command is available and set the state variables accordingly.
         checkAllCommandsAvailability(dependencies)
-          .then((isAvailable) => {
-            // Set the command availability state.
-            setIsCommandAvailable(isAvailable); 
-            // Set the modal state to opened if the command is not available.
-            setOpened(!isAvailable); 
-            // Set loading to false after the check is done.
-            setLoadingModal(false); // Ensure this is set to false even if successful
-          })
-          .catch((error) => {
-            console.error("An error occurred:", error);
-            // Also set loading to false in case of error.
-            setLoadingModal(false); // Crucial change, set to false even on errors
-          });
-      }, []);
+            .then((isAvailable) => {
+                // Set the command availability state.
+                setIsCommandAvailable(isAvailable);
+                // Set the modal state to opened if the command is not available.
+                setOpened(!isAvailable);
+                // Set loading to false after the check is done.
+                setLoadingModal(false); // Ensure this is set to false even if successful
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
+                // Also set loading to false in case of error.
+                setLoadingModal(false); // Crucial change, set to false even on errors
+            });
+    }, []);
 
-
-     /**
+    /**
      * handleProcessData: Callback to handle and append new data from the child process to the output.
      * It updates the state by appending the new data received to the existing output.
      * @param {string} data - The data received from the child process.
@@ -80,7 +82,7 @@ export function ShodanAPITool() {
         setOutput((prevOutput) => prevOutput + "\n" + data); // Update output.
     }, []);
 
-     /**
+    /**
      * handleProcessTermination: Callback to handle the termination of the child process.
      * Once the process termination is handled, it clears the process PID reference and
      * deactivates the loading overlay.
@@ -106,10 +108,8 @@ export function ShodanAPITool() {
             setAllowSave(true);
             setHasSaved(false);
         },
-        [handleProcessData]
+        [handleProcessData],
     );
-
-  
 
     /**
      * handleSaveComplete: Callback to handle the completion of the file saving process.
@@ -121,7 +121,7 @@ export function ShodanAPITool() {
         setHasSaved(true);
         setAllowSave(false);
     };
-    
+
     /**
      * onSubmit: Asynchronous handler for the form submission event.
      * It sets up and triggers the Shodan API tool with the given parameters.
@@ -136,7 +136,7 @@ export function ShodanAPITool() {
         // Enable the loading overlay while the tool executes.
         setLoading(true);
 
-            const args = ["/usr/share/ddt/shodkey.py", "-i", values.hostIP, "-k", values.shodanKey];
+        const args = ["/usr/share/ddt/shodkey.py", "-i", values.hostIP, "-k", values.shodanKey];
 
         try {
             // Execute the Shodan command via helper method and handle its output or potential errors.
@@ -144,12 +144,11 @@ export function ShodanAPITool() {
                 "python3",
                 args,
                 handleProcessData,
-                handleProcessTermination
+                handleProcessTermination,
             );
             //Update command with the result.
             setPid(result.pid);
             setOutput(result.output);
-
         } catch (e: any) {
             //Display output error messages.
             setOutput(e.message);
@@ -167,39 +166,34 @@ export function ShodanAPITool() {
     }, [setOutput]);
 
     return (
-
         <>
-        <RenderComponent
-          title={title}
-          description={description}
-          steps={steps}
-          tutorial={tutorial}
-          sourceLink={sourceLink}
-
-     >     {!loadingModal && (
-             <InstallationModal
-              isOpen={opened}
-              setOpened={setOpened}
-              feature_description={description}
-              dependencies={dependencies}
-         ></InstallationModal>
-    )}
-
-       
-         <form onSubmit={form.onSubmit(onSubmit)}>
-             <Stack>
-                {LoadingOverlayAndCancelButton(loading, pid)}
-                <TextInput label={"Valid API Key"} required {...form.getInputProps("shodanKey")} />
-                <TextInput label={"Host IP"} required {...form.getInputProps("hostIP")} />
-                <Button type={"submit"}>Scan</Button>
-                 {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
-                <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
-            </Stack>
-        </form>
-     </RenderComponent>
-
-    </>
-
+            <RenderComponent
+                title={title}
+                description={description}
+                steps={steps}
+                tutorial={tutorial}
+                sourceLink={sourceLink}
+            >
+                {" "}
+                {!loadingModal && (
+                    <InstallationModal
+                        isOpen={opened}
+                        setOpened={setOpened}
+                        feature_description={description}
+                        dependencies={dependencies}
+                    ></InstallationModal>
+                )}
+                <form onSubmit={form.onSubmit(onSubmit)}>
+                    <Stack>
+                        {LoadingOverlayAndCancelButton(loading, pid)}
+                        <TextInput label={"Valid API Key"} required {...form.getInputProps("shodanKey")} />
+                        <TextInput label={"Host IP"} required {...form.getInputProps("hostIP")} />
+                        <Button type={"submit"}>Scan</Button>
+                        {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                        <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+                    </Stack>
+                </form>
+            </RenderComponent>
+        </>
     );
-};
-    
+}
