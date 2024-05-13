6fd9a192eb077e40ccc1369269ce48fb850f4585
SUA-321 <ubaid.a218@gmail.com>
2024-05-13 20:37:27 +1000

    reworked user guide/description

--- src/components/DirbTool/DirbTool.tsx
+++ src/components/DirbTool/DirbTool.tsx
@@ -20,14 +20,14 @@ interface FormValuesType {
 
 const title = "Dirb"; //Title of the component.
 const description_userGuide = //Description of the component. 
-    "Dirb is a Web Content Scanner that acts to seek out any existing or hidden Web Objects. " +
+    "Dirb is a Web Content Scanner. It looks for existing (and/or hidden) Web Objects. " +
     "This is a dictionary-based attack that takes place upon a web server and will analyse the " +
     "results within this process.\n\nHow to use Dirb:\n\nStep 1: Enter a valid URL.\n" +
     "       E.g. https://www.deakin.edu.au\n\nStep 2: Enter a file directory pathway to access " +
     "a wordlist\n       E.g. home/wordlist/wordlist.txt\n\nStep 3: Click Scan to commence " +
     "the Dirb operation.\n\nStep 4: View the Output block below to view the results of the tool's execution.";
 
-
+    
 /**
  * The DirbTool component.
  * @returns The DirbTool component.
