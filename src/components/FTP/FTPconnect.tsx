82a919a5f400c6693bc75d9bb1564d1da389cfc2
connorwjordan <connorwilljordan@gmail.com>
2024-08-04 14:04:59 +1000

    fix: imported appropriate libraries and added component constants to FTPconnect function

--- src/components/FTP/FTPconnect.tsx
+++ src/components/FTP/FTPconnect.tsx
@@ -3,7 +3,6 @@ import { useForm } from "@mantine/form";
 import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
-import { UserGuide } from "../UserGuide/UserGuide";
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 
