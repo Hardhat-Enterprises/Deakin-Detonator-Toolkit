d8e6d177299ee077f29f40c44d5d452a1bfa9b42
slpysiren <slpyneris@gmail.com>
2024-08-05 19:41:54 +1000

    Add option to generateFileName to potentially make it usable for output saving

--- src/components/FileHandler/FileHandler.tsx
+++ src/components/FileHandler/FileHandler.tsx
@@ -1,10 +1,11 @@
 import { invoke } from "@tauri-apps/api";
 import { v4 as uuidv4 } from "uuid";
 
-export function generateFileName(fileName: string): string {
+export function generateFileName(fileName: string, output: boolean = false): string {
     const dateTime = new Date().toISOString().replace(/[-:.]/g, "");
     const uniqueString = uuidv4();
-    const generatedFileName = `${fileName}_${dateTime}_${uniqueString}`;
+    let generatedFileName = `${fileName}_${dateTime}_${uniqueString}`;
+    generatedFileName += output ? "output.txt" : "";
     return generatedFileName;
 }
 
