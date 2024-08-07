94feadb8aac36de65e08ee1ff2f8ee480c20703a
SethJ1 <sethj4774@gmail.com>
2024-08-07 12:46:18 +1000

    libraries

--- src/components/Whois/Whois.tsx
+++ src/components/Whois/Whois.tsx
@@ -1,3 +1,14 @@
+import { useState, useEffect, useCallback } from "react";
+import { Button, Checkbox, Stack, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { CommandHelper } from "../../utils/CommandHelper";
+import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
+import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
+import { RenderComponent } from "../UserGuide/UserGuide";
+import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
+import InstallationModal from "../InstallationModal/InstallationModal";
+import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
+
 function Whois() {
     return null
 }
