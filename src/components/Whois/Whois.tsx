7e18061a0b0f982d3936dd26b61eaadabc2e58d5
SethJ1 <sethj4774@gmail.com>
2024-08-07 12:53:17 +1000

    componenet constants and state variables.

--- src/components/Whois/Whois.tsx
+++ src/components/Whois/Whois.tsx
@@ -16,8 +16,34 @@ interface FormValuesType {
     targetURL: string;
 }
 
+/**
+ * The Whois component.
+ * @returns The Whois component.
+ */
 function Whois() {
-    return null
+    // Component state variables
+    const [loading, setLoading] = useState(false); // State variable to indicate loading state
+    const [output, setOutput] = useState(""); // State variable to store the output of the command execution
+    const [allowSave, setAllowSave] = useState(false); // State variable to allow saving of output
+    const [hasSaved, setHasSaved] = useState(false); // State variable to indicate if output has been saved
+    const [sslScan, setSslScan] = useState(false); // State variable for SSL scanning
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
+    const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
+    const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
+    const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
+
+    // Component Constants
+    const title = "Whois";
+    const description =
+        "Whois is a query and response protocol that is used for querying databases that store an Internet resource's registered users or assignees.";
+    const steps =
+        "Step 1: Provide the target URL or IP address to scan.\n" +
+        "Step 2: Start the scan to gather information about potential vulnerabilities and misconfigurations.\n" +
+        "Step 3: Review the scan output to identify any security issues.\n";
+    const sourceLink = "https://github.com/weppos/whois"; // Link to the source code
+    const tutorial = ""; // Link to the official documentation/tutorial
+    const dependencies = ["whois"]; // Contains the dependencies required by the component.   
+
 }
 export default Whois;
 
