ee336dbe348d953f29506794bb30d597fbd50d1e
anjojoseph <anjojosephapj@gmailcom>
2024-08-07 17:42:11 -0400

    docstrings added, inline comments added and steps and description formatted

--- src/components/Cewl/Cewl.tsx
+++ src/components/Cewl/Cewl.tsx
@@ -19,28 +19,31 @@ interface FormValuesType {
     username: string;
     password: string;
     wordList: string;
-
 }
+
 // Component Constants.
-const title = "Cewl";
-const description_userguide =
+const title = "Cewl";// Title of the component.
+const description =
     "The tool Cewl, renown for being a Custom Word List Generator, is a ruby app which spiders given URLs to " +
     "a specified depth to return a list of words that are able to be used within password crackers including " +
     " JohnTheRipper (which can be found within the DDT). This tool is particularly useful for security testing and " +
     "forensic investigation.\n\nOptions for the tool can be found at:  https://www.kali.org/tools/cewl/#:~:text=CeWL" +
-    "%20(Custom%20Word%20List%20generator,\nCeWL%20can%20follow%20external%20links.\n\n" +
-    "Using Cewl\n" +
+    "%20(Custom%20Word%20List%20generator,\nCeWL%20can%20follow%20external%20links.";// Description of the component.
+const steps= 
     "Step 1: Enter the Maximum depth to spider to.\n" +
-    "       Eg: 2\n\n" +
+    "       Eg: 2\n" +
     "Step 2: Enter a Minimum word length.\n" +
-    "       Eg: 3\n\n" +
+    "       Eg: 3\n" +
     "Step 3: Enter a Target URL.\n" +
-    "       Eg: google.com\n\n" +
-    "Step 4: Click Scan to commence Cewl's operation.\n\n" +
+    "       Eg: google.com\n" +
+    "Step 4: Click Scan to commence Cewl's operation.\n" +
     "Step 5: View the Output block below to view the results of the tools execution.";
-
 const methods = ["No authentication", "Basic", "Digest"];
 
+/**
+ * The Cewl component.
+ * @returns The Cewl component.
+ */
 const Cewl = () => {
     const [loading, setLoading] = useState(false);
     const [output, setOutput] = useState("");
@@ -54,6 +57,7 @@ const Cewl = () => {
     const [allowSave, setAllowSave] = useState(false);
     const [hasSaved, setHasSaved] = useState(false);
 
+    // Form hook to handle form input.
     let form = useForm({
         initialValues: {
             depth: "",
@@ -65,31 +69,48 @@ const Cewl = () => {
             wordList: "",
         },
     });
+    /** *
+     * handleProcessData: Callback to handle and append new data from the child process to the output.
+     *  It updates the state by appending the new data received to the existing output.
+     *  @param {string} data - The data received from the child process.
+     * */
     const handleProcessData = useCallback((data: string) => {
-        setOutput((prevOutput) => prevOutput + "\n" + data); // Update output
+        setOutput((prevOutput) => prevOutput + "\n" + data); // Append new data to the previous output.
     }, []);
-    // Uses the onTermination callback function of runCommandGetPidAndOutput to handle
-    // the termination of that process, resetting state variables, handling the output data,
-    // and informing the user.
+
+    /**
+     * handleProcessTermination: Callback to handle the termination of the child process.
+     * Once the process termination is handled, it clears the process PID reference and
+     * deactivates the loading overlay.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
+     */
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
+            // If the process was successful, display a success message.
             if (code === 0) {
                 handleProcessData("\nProcess completed successfully.");
+
+                // If the process was terminated manually, display a termination message.
             } else if (signal === 15) {
                 handleProcessData("\nProcess was manually terminated.");
+
+                // If the process was terminated with an error, display the exit and signal codes.
             } else {
                 handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
             }
+
             // Clear the child process pid reference
             setPid("");
+
             // Cancel the Loading Overlay
             setLoading(false);
-
             // Allow Saving as the output is finalised
             setAllowSave(true);
             setHasSaved(false);
         },
-        [handleProcessData]
+        [handleProcessData] // Dependency on the handleProcessData callback
     );
     // Sends a SIGTERM signal to gracefully terminate the process
     const handleCancel = () => {
@@ -106,12 +127,22 @@ const Cewl = () => {
         setHasSaved(true);
         setAllowSave(false);
     };
-
+    
+     /**
+     * onSubmit: Asynchronous handler for the form submission event.
+     * It sets up and triggers the cewl tool with the given parameters.
+     * Once the command is executed, the results or errors are displayed in the output.
+     *
+     * @param {FormValuesType} values - The form values, containing the depth, minLength, maxLength, url, authType, username, password and wordList.
+     */
     const onSubmit = async (values: FormValuesType) => {
+        // Activate loading state to indicate ongoing process
+        setLoading(true);
+
         // Disallow saving until the tool's execution is complete
         setAllowSave(false);
 
-        setLoading(true);
+        // Construct arguments for the cewl command based on form input
         const args = [];
         if (values.depth) {
             args.push("-d", `${values.depth}`);
@@ -149,7 +180,7 @@ const Cewl = () => {
         if (values.wordList) {
             args.push("-w", `${values.wordList}`);
         }
-
+        // Execute the cewl command via helper method and handle its output or potential errors
         try {
             const result = await CommandHelper.runCommandGetPidAndOutput(
                 "cewl",
@@ -157,12 +188,20 @@ const Cewl = () => {
                 handleProcessData,
                 handleProcessTermination
             );
+            // Update the UI with the results from the executed command
             setPid(result.pid);
             setOutput(result.output);
+            // Enable setAllowSave to generate output file
+            setAllowSave(true);
         } catch (e: any) {
             setOutput(e.message);
+            // Disallow save after the output
+            setAllowSave(false);
         }
     };
+    /**
+     * Clears the output state.
+     */
     const clearOutput = useCallback(() => {
         setOutput("");
         setHasSaved(false);
@@ -182,7 +221,7 @@ const Cewl = () => {
                 </div>
             )}
             <Stack>
-                {UserGuide(title, description_userguide)}
+                {UserGuide(title, description)}
                 <Switch
                     size="md"
                     label="Advanced Mode"
