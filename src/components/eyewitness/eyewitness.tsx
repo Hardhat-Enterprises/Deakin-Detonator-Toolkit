940ac14e576241803f41a4dc60108c1d0e65e3dc
uppowerdo <serves.boasts.0p@icloud.com>
2024-08-24 01:16:21 -0400

    import title from mantine

--- src/components/eyewitness/eyewitness.tsx
+++ src/components/eyewitness/eyewitness.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, TextInput } from "@mantine/core";
+import { Button, Stack, TextInput, Title } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
