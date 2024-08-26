6b17c1512b6eed00792fb34c3cd8c9eae7240d96
hathimsh <166583896+hathimsh@users.noreply.github.com>
2024-08-25 23:03:40 -0400

    Revert "Updated ns lookup tool"

--- src/components/NSLookupTool/NSLookupTool.tsx
+++ src/components/NSLookupTool/NSLookupTool.tsx
@@ -1,75 +1,56 @@
 import { Button, Stack, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
-import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
-import InstallationModal from "../InstallationModal/InstallationModal";
-import { RenderComponent } from "../UserGuide/UserGuide";
+import { UserGuide } from "../UserGuide/UserGuide";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
-import { installDependencies } from "../../utils/InstallHelper";
 
-/**
- * Title and Description of the tool
- */
-const title = "NSLookup";
-const description =
-    "The NSLookup command is a tool used to query Domain Name System (DNS) servers and retrieve information about a specific domain or IP address. This command is an essential tool for network administrators and system engineers as it can be used to troubleshoot DNS issues and gather information about DNS configurations.";
-const steps =
+const title = "NsLookup";
+const description_userguide =
+    "The nslookup command is a tool used to query Domain Name System (DNS) servers and retrieve information about a specific domain or IP address." +
+    "This command is an essential tool for network administrators and system engineers as it can be used to troubleshoot DNS issues and gather information about DNS configurations." +
     "How to use NSLookUp.\n\n" +
     "Step 1: Enter an IP or Web URL.\n" +
-    " E.g. 127.0.0.1\n\n" +
+    "       E.g. 127.0.0.1\n\n" +
     "Step 2: View the Output block below to view the results of the Scan.";
-const tutorial = "";
-const sourceLink = "https://www.nslookup.io";
 
-// Form Value Interface
-interface FormValuesType {
-    ipAddress: string; // Ip Address that needs to be looked up
-    tutorial: string; //Tutorial Text
+interface FormValues {
+    ipaddress: string;
 }
 
-function NSLookup() {
-    // State the Variables
-    const [loading, setLoading] = useState(false); // Indication of running the process
-    const [pid, setPid] = useState(""); // Process ID
-    const [output, setOutput] = useState(""); // Output
-    const [allowSave, setAllowSave] = useState(false); // Looking whether the process can be saved
-    const [hasSaved, setHasSaved] = useState(false); // Indication of saved process
-    const [opened, setOpened] = useState(false); // Modal open state
-    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // Command Availability state
-    const [loadingModal, setLoadingModal] = useState(true); // Loading state for modal
-    const dependencies = ["bind9"];
+export function NSLookup() {
+    const [loading, setLoading] = useState(false);
+    const [pid, setPid] = useState("");
+    const [output, setOutput] = useState("");
+    const [allowSave, setAllowSave] = useState(false);
+    const [hasSaved, setHasSaved] = useState(false);
 
-    // Check if the command is available and set the state variables accordingly.
-    useEffect(() => {
-        checkAllCommandsAvailability(dependencies)
-            .then((isAvailable) => {
-                setIsCommandAvailable(isAvailable);
-                setOpened(!isAvailable);
-                setLoadingModal(false);
-            })
-            .catch((error) => {
-                console.error("An error occurred:", error);
-                setLoadingModal(false);
-            });
-    }, []);
-
-    // Form Hook to handle form input
-    const form = useForm({
+    let form = useForm({
         initialValues: {
-            ipAddress: "",
-            tutorial: "",
+            ipaddress: "",
         },
     });
 
-    // Handling the processed data output
+    /**
+     * handleProcessData: Callback to handle and append new data from the child process to the output.
+     * It updates the state by appending the new data received to the existing output.
+     *
+     * @param {string} data - The data received from the child process.
+     */
     const handleProcessData = useCallback((data: string) => {
-        setOutput((prevOutput) => prevOutput + "\n" + data);
+        setOutput((prevOutput) => prevOutput + "\n" + data); // Append new data to the previous output.
     }, []);
 
-    // Process Termination
+    /**
+     * handleProcessTermination: Callback to handle the termination of the child process.
+     * Once the process termination is handled, it clears the process PID reference and
+     * deactivates the loading overlay.
+     * @param {object} param0 - An object containing information about the process termination.
+     * @param {number} param0.code - The exit code of the terminated process.
+     * @param {number} param0.signal - The signal code indicating how the process was terminated.
+     */
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
             if (code === 0) {
@@ -79,32 +60,40 @@ function NSLookup() {
             } else {
                 handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
             }
+            // Clear the child process pid reference
             setPid("");
+            // Cancel the Loading Overlay
             setLoading(false);
+
+            // Allow Saving as the output is finalised
             setAllowSave(true);
             setHasSaved(false);
         },
-        [handleProcessData]
+        [handleProcessData] // Dependency on the handleProcessData callback
     );
+    /**
+     * onSubmit: Handler function that is triggered when the form is submitted.
+     * It prepares the arguments and initiates the execution of the `bed` command.
+     * Upon successful execution, it updates the state with the process PID and output.
+     * If an error occurs during the command execution, it updates the output with the error message.
+     * @param {FormValues} values - An object containing the form input values.
+     */
 
-    // Updating the state of Saving the output
+    // Actions taken after saving the output
     const handleSaveComplete = () => {
+        // Indicating that the file has saved which is passed
+        // back into SaveOutputToTextFile to inform the user
         setHasSaved(true);
         setAllowSave(false);
     };
 
-    // Handling the submitted form
-    const onSubmit = (values: FormValuesType) => {
-        const ipAddress = values.ipAddress.trim();
-        if (!ipAddress) {
-            setOutput("Error: No IP address or hostname provided.");
-            return;
-        }
-
+    const onSubmit = (values: FormValues) => {
+        // Disallow saving until the tool's execution is complete
         setAllowSave(false);
-        setLoading(true);
 
-        CommandHelper.runCommandGetPidAndOutput("nslookup", [ipAddress], handleProcessData, handleProcessTermination)
+        setLoading(true);
+        const args = ["-s", values.ipaddress];
+        CommandHelper.runCommandGetPidAndOutput("nslookup", args, handleProcessData, handleProcessTermination)
             .then(({ pid, output }) => {
                 setPid(pid);
                 setOutput(output);
@@ -115,50 +104,31 @@ function NSLookup() {
             });
     };
 
-    // Clearing the output
+    /**
+     * clearOutput: A callback function to clear the current output displayed in the UI.
+     * This function resets the output state to an empty string.
+     */
     const clearOutput = useCallback(() => {
         setOutput("");
         setHasSaved(false);
         setAllowSave(false);
-    }, [setOutput]);
+    }, [setOutput]); // Dependency on the setOutput function.
 
     return (
-        <>
-            <RenderComponent
-                title={title}
-                description={description}
-                steps={steps}
-                tutorial={tutorial}
-                sourceLink={sourceLink}
-                children={""}
-            />
-
-            {!loadingModal && (
-                <InstallationModal
-                    isOpen={opened}
-                    setOpened={setOpened}
-                    feature_description={description}
-                    dependencies={dependencies}
+        <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
+            {LoadingOverlayAndCancelButton(loading, pid)}
+            <Stack>
+                {UserGuide(title, description_userguide)}
+                <TextInput
+                    label={"Please enter the IP Address for nslookup"}
+                    required
+                    {...form.getInputProps("ipaddress")}
                 />
-            )}
-            <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
-                {LoadingOverlayAndCancelButton(loading, pid)}
-                <Stack>
-                    <TextInput
-                        label={"Please enter the IP Address for NSLookup"}
-                        required
-                        {...form.getInputProps("ipAddress")}
-                    />
-
-                    <TextInput label={"Tutorial"} {...form.getInputProps("tutorial")} />
-
-                    {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
-                    <Button type={"submit"}>Start {title}</Button>
-                    <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
-                </Stack>
-            </form>
-        </>
+                {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                <Button type={"submit"}>Scan</Button>
+                <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+            </Stack>
+        </form>
     );
 }
-
 export default NSLookup;
