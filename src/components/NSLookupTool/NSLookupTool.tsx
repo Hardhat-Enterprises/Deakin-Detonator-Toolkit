3d44466835e1340aa794298bbabc786fec031ccc
hathimsh <hathimshahul@gmail.com>
2024-08-25 22:52:18 -0400

    Done the changes as suggested

--- src/components/NSLookupTool/NSLookupTool.tsx
+++ src/components/NSLookupTool/NSLookupTool.tsx
@@ -1,23 +1,28 @@
 import { Button, Stack, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
-import { UserGuide } from "../UserGuide/UserGuide";
+import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
+import InstallationModal from "../InstallationModal/InstallationModal";
+import { RenderComponent } from "../UserGuide/UserGuide";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
+import { installDependencies } from "../../utils/InstallHelper";
 
 /**
  * Title and Description of the tool
  */
 const title = "NSLookup";
-const description_Userguide =
-    "The NSLookup command is a tool used to query Domain Name System (DNS) servers and retrieve information about a specific domain or IP address." +
-    "This command is an essential tool for network administrators and system engineers as it can be used to troubleshoot DNS issues and gather information about DNS configurations." +
+const description =
+    "The NSLookup command is a tool used to query Domain Name System (DNS) servers and retrieve information about a specific domain or IP address. This command is an essential tool for network administrators and system engineers as it can be used to troubleshoot DNS issues and gather information about DNS configurations.";
+const steps =
     "How to use NSLookUp.\n\n" +
     "Step 1: Enter an IP or Web URL.\n" +
     " E.g. 127.0.0.1\n\n" +
     "Step 2: View the Output block below to view the results of the Scan.";
+const tutorial = "";
+const sourceLink = "https://www.nslookup.io";
 
 // Form Value Interface
 interface FormValuesType {
@@ -25,36 +30,46 @@ interface FormValuesType {
     tutorial: string; //Tutorial Text
 }
 
-export function NSLookup() {
+function NSLookup() {
     // State the Variables
-
     const [loading, setLoading] = useState(false); // Indication of running the process
     const [pid, setPid] = useState(""); // Process ID
     const [output, setOutput] = useState(""); // Output
     const [allowSave, setAllowSave] = useState(false); // Looking whether the process can be saved
     const [hasSaved, setHasSaved] = useState(false); // Indication of saved process
+    const [opened, setOpened] = useState(false); // Modal open state
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // Command Availability state
+    const [loadingModal, setLoadingModal] = useState(true); // Loading state for modal
+    const dependencies = ["bind9"];
 
-    // Form management by using Mantine
+    // Check if the command is available and set the state variables accordingly.
+    useEffect(() => {
+        checkAllCommandsAvailability(dependencies)
+            .then((isAvailable) => {
+                setIsCommandAvailable(isAvailable);
+                setOpened(!isAvailable);
+                setLoadingModal(false);
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
+                setLoadingModal(false);
+            });
+    }, []);
+
+    // Form Hook to handle form input
     const form = useForm({
         initialValues: {
-            ipAddress: "", // Initial Value of IP address
-            tutorial: "", // Initial Value of Tutorial
+            ipAddress: "",
+            tutorial: "",
         },
     });
 
     // Handling the processed data output
-    // @param {string} data - Data output from the process
-
     const handleProcessData = useCallback((data: string) => {
         setOutput((prevOutput) => prevOutput + "\n" + data);
-    }, []); // Handles the termination and provides the apporpritate message
+    }, []);
 
     // Process Termination
-    //Once the process termination is handled, it clears the process PID reference and deactivates the loading overlay.
-    //@param {Object} param - Object containing the termination code and signal
-    //@param {number} param.code - Termination code of the process
-    //@param {number} param.signal - Signal code of the process
-
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
             if (code === 0) {
@@ -64,70 +79,86 @@ export function NSLookup() {
             } else {
                 handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
             }
-            setPid(""); // Reset the Process ID
-            setLoading(false); // Stop loading
-            setAllowSave(true); // Allows the saving of output
-            setHasSaved(false); // Reset save status
+            setPid("");
+            setLoading(false);
+            setAllowSave(true);
+            setHasSaved(false);
         },
         [handleProcessData]
     );
 
     // Updating the state of Saving the output
-
     const handleSaveComplete = () => {
-        setHasSaved(true); // Mark output as saved
-        setAllowSave(false); // Disabling the Save option
+        setHasSaved(true);
+        setAllowSave(false);
     };
 
     // Handling the submitted form
-
     const onSubmit = (values: FormValuesType) => {
-        const ipAddress = values.ipAddress.trim(); // Removing the extra white space
+        const ipAddress = values.ipAddress.trim();
         if (!ipAddress) {
-            setOutput("Error: No IP address or hostname provided."); // Error Message
+            setOutput("Error: No IP address or hostname provided.");
             return;
         }
 
-        setAllowSave(false); // Disabling the save option before start
-        setLoading(true); // Show loading indication
+        setAllowSave(false);
+        setLoading(true);
 
         CommandHelper.runCommandGetPidAndOutput("nslookup", [ipAddress], handleProcessData, handleProcessTermination)
             .then(({ pid, output }) => {
-                setPid(pid); // Setting the Process ID
-                setOutput(output); // Setting the command output
+                setPid(pid);
+                setOutput(output);
             })
             .catch((error) => {
-                setLoading(false); //Stop Loading
-                setOutput(`Error: ${error.message}`); // Display the error message
+                setLoading(false);
+                setOutput(`Error: ${error.message}`);
             });
     };
 
     // Clearing the output
-
     const clearOutput = useCallback(() => {
-        setOutput(""); // Clears output
-        setHasSaved(false); // Reset save status
-        setAllowSave(false); // Disable saving
+        setOutput("");
+        setHasSaved(false);
+        setAllowSave(false);
     }, [setOutput]);
 
     return (
-        <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
-            {LoadingOverlayAndCancelButton(loading, pid)}
-            <Stack>
-                {UserGuide(title, description_Userguide)}
-                <TextInput
-                    label={"Please enter the IP Address for NSLookup"}
-                    required
-                    {...form.getInputProps("ipAddress")}
+        <>
+            <RenderComponent
+                title={title}
+                description={description}
+                steps={steps}
+                tutorial={tutorial}
+                sourceLink={sourceLink}
+                children={""}
+            />
+
+            {!loadingModal && (
+                <InstallationModal
+                    isOpen={opened}
+                    setOpened={setOpened}
+                    feature_description={description}
+                    dependencies={dependencies}
                 />
+            )}
+            <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
+                {LoadingOverlayAndCancelButton(loading, pid)}
+                <Stack>
+                    <TextInput
+                        label={"Please enter the IP Address for NSLookup"}
+                        required
+                        {...form.getInputProps("ipAddress")}
+                    />
 
-                <TextInput label={"Tutorial"} {...form.getInputProps("tutorial")} />
+                    <TextInput label={"Tutorial"} {...form.getInputProps("tutorial")} />
 
-                {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
-                <Button type={"submit"}>Scan</Button>
-                <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
-            </Stack>
-        </form>
+                    {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                    <Button type={"submit"}>Start {title}</Button>
+                    <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+                </Stack>
+            </form>
+        </>
     );
 }
+
 export default NSLookup;
