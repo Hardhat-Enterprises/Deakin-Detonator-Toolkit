722301db207d4111a419637d4d755ee1ee54fb96
basoreeper3007 <Sebastian.rao3007@gmail.com>
2024-08-28 04:33:30 -0400

    Updated to a more suitable template code

--- src/components/Masscan/Masscan.tsx
+++ src/components/Masscan/Masscan.tsx
@@ -1,75 +1,147 @@
-import { Button, NativeSelect, Stack, TextInput } from "@mantine/core";
+import { useState, useEffect, useCallback } from "react";
+import { Button, Stack, TextInput, Checkbox } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { RenderComponent } from "../UserGuide/UserGuide";
+import InstallationModal from "../InstallationModal/InstallationModal";
+import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
+import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
 
-// Component Constants.
-const title = "Masscan"; // Title of the component.
-const description = "Masscan is a tool used to..."; // Description of the component.
-const steps =
-    "Step 1: ...\n" +
-    "Step 2: ...";
-const sourceLink = ""; // Link to the source code (or Kali Tools).
-const tutorial = ""; // Link to the official documentation/tutorial.
-const dependencies = ["masscan"]; // Contains the dependencies required by the component.
-
-//Variables
-interface FormValuesType { //Relevant form values to be added as Masscan options are added
-    masscanOptions: string; 
+/**
+ * Represents the form values for the Masscan component.
+ */
+interface FormValuesType {
+    input1: string;
 }
 
-//Masscan Options
-const masscanOptions = [ //More options to be added (version check could be kept?)
-    "Version Check"
-];
-
+/**
+ * The Masscan component.
+ * @returns The Masscan component.
+ */
 const Masscan = () => {
-    var [output, setOutput] = useState("");
-    const [selectedMasscanOption, setSelectedMasscanOption] = useState("");
+    // Component state variables
+    const [loading, setLoading] = useState(false); // State variable to indicate loading state
+    const [output, setOutput] = useState(""); // State variable to store the output of the command execution
     const [allowSave, setAllowSave] = useState(false); // State variable to allow saving of output
     const [hasSaved, setHasSaved] = useState(false); // State variable to indicate if output has been saved
-    const [loading, setLoading] = useState(false); // State variable to indicate loading state
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
+    const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
+    const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
+    const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
+    const [verboseMode, setVerboseMode] = useState(false); // State variable for verbose mode
 
-    let form = useForm({ //Relevant form values to be added as Masscan options are added
+    // Component Constants
+    const title = "Masscan";
+    const description = "Masscan is a...";
+    const steps =
+        "Step 1: \n" +
+        "Step 2: \n" +
+        "Step 3: \n";
+    const sourceLink = ""; // Link to the source code
+    const tutorial = ""; // Link to the official documentation/tutorial
+    const dependencies = ["masscan"]; // Contains the dependencies required by the component.
+
+    // Form hook to handle form input
+    let form = useForm({
         initialValues: {
             input1: "",
         },
     });
 
+    useEffect(() => {
+        // Check if the command is available and set the state variables accordingly.
+        checkAllCommandsAvailability(dependencies)
+            .then((isAvailable) => {
+                setIsCommandAvailable(isAvailable); // Set the command availability state
+                setOpened(!isAvailable); // Set the modal state to opened if the command is not available
+                setLoadingModal(false); // Set loading to false after the check is done
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
+                setLoadingModal(false); // Also set loading to false in case of error
+            });
+    }, []);
+
+    /**
+     * handleProcessData: Callback to handle and append new data from the child process to the output.
+     * It updates the state by appending the new data received to the existing output.
+     * @param {string} data - The data received from the child process.
+     */
+    const handleProcessData = useCallback((data: string) => {
+        setOutput((prevOutput) => prevOutput + "\n" + data); // Append new data to the previous output.
+    }, []);
+
+    /**
+     * handleProcessTermination: Callback to handle the termination of the child process.
+     * Once the process termination is handled, it clears the process PID reference and
+     * deactivates the loading overlay.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
+     */
+    const handleProcessTermination = useCallback(
+        ({ code, signal }: { code: number; signal: number }) => {
+            // If the process was successful, display a success message.
+            if (code === 0) {
+                handleProcessData("\nProcess completed successfully.");
+
+                // If the process was terminated manually, display a termination message.
+            } else if (signal === 15) {
+                handleProcessData("\nProcess was manually terminated.");
+
+                // If the process was terminated with an error, display the exit and signal codes.
+            } else {
+                handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
+            }
+
+            // Clear the child process pid reference. There is no longer a valid process running.
+            setPid("");
+
+            // Cancel the loading overlay. The process has completed.
+            setLoading(false);
+        },
+        [handleProcessData] // Dependency on the handleProcessData callback
+    );
+
+    /**
+     * Handles form submission for the Masscan component.
+     * @param {FormValuesType} values - The form values containing the target domain.
+     */
     const onSubmit = async (values: FormValuesType) => {
-        // Set loading state to true and disallow output saving
+        // Activate loading state to indicate ongoing process
         setLoading(true);
-        setAllowSave(false);
 
-        let args = [``];
-
-        //Switch case
-        switch (values.masscanOptions) {
-            case "Version Check": //This version check option is a baseline to test that Masscan functions
-                args = [`--version`];
-
-                try {
-                    let output = await CommandHelper.runCommand("masscan", args);
-                    setOutput(output);
-                } catch (e: any) {
-                    setOutput(e);
-                } finally{
-                    // Set loading state to false and allow output saving
-                    setLoading(false);
-                    setAllowSave(true);
-                }
-
-                break;
+        // Construct arguments for the Masscan command based on form input
+        const args = ["-p", values.input1];
+        if (verboseMode) {
+            args.push("-v"); // Add verbose mode option if enabled
         }
+
+        // Execute the Masscan command via helper method and handle its output or potential errors
+        CommandHelper.runCommandGetPidAndOutput(
+            "masscan",
+            [...args, "--no-color"],
+            handleProcessData,
+            handleProcessTermination
+        )
+            .then(() => {
+                // Deactivate loading state
+                setLoading(false);
+            })
+            .catch((error) => {
+                // Display any errors encountered during command execution
+                setOutput(`Error: ${error.message}`);
+                // Deactivate loading state
+                setLoading(false);
+            });
     };
 
-     /**
+    /**
      * Handles the completion of output saving by updating state variables.
      */
-     const handleSaveComplete = () => {
+    const handleSaveComplete = () => {
         setHasSaved(true); // Set hasSaved state to true
         setAllowSave(false); // Disallow further output saving
     };
@@ -83,7 +155,7 @@ const Masscan = () => {
         setAllowSave(false); // Disallow further output saving
     };
 
-    //<ConsoleWrapper output={output} clearOutputCallback={clearOutput} /> prints the terminal on the tool
+    // Render component
     return (
         <RenderComponent
             title={title}
@@ -92,24 +164,28 @@ const Masscan = () => {
             tutorial={tutorial}
             sourceLink={sourceLink}
         >
-        <form onSubmit={form.onSubmit((values) => onSubmit({ ...values, masscanOptions: selectedMasscanOption }))}>
-            <Stack>
-                
-                <NativeSelect
-                    value={selectedMasscanOption}
-                    onChange={(e) => setSelectedMasscanOption(e.target.value)}
-                    title={"Masscan option"}
-                    data={masscanOptions}
-                    required
-                    placeholder={"Pick a Masscan option"}
-                    description={"Type of action to perform"}
+            {!loadingModal && (
+                <InstallationModal
+                    isOpen={opened}
+                    setOpened={setOpened}
+                    feature_description={description}
+                    dependencies={dependencies}
                 />
-                <TextInput label={"Input 1"} {...form.getInputProps("input1")} />
-                <Button type={"submit"}>start masscan</Button>
-                {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
-                <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
-            </Stack>
-        </form>
+            )}
+            <form onSubmit={form.onSubmit(onSubmit)}>
+                <Stack>
+                    {LoadingOverlayAndCancelButton(loading, pid)}
+                    <TextInput label="input1" required {...form.getInputProps("input1")} />
+                    <Checkbox
+                        label="Verbose Mode"
+                        checked={verboseMode}
+                        onChange={(event) => setVerboseMode(event.currentTarget.checked)}
+                    />
+                    <Button type={"submit"}>Start {title}</Button>
+                    {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                    <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+                </Stack>
+            </form>
         </RenderComponent>
     );
 };
