afe3c39ffc01dda3150db6a5679c59ce7242e558
SethJ1 <sethj4774@gmail.com>
2024-08-31 15:03:15 +1000

    added comments and ran prettier

--- src/components/Unicornscan/Unicornscan.tsx
+++ src/components/Unicornscan/Unicornscan.tsx
@@ -44,7 +44,8 @@ const Unicornscan = () => {
     // Component Constants
     const title = "Unicornscan";
     const description = "Unicornscan is a tool used to gather information about systems and services on a network.";
-    const steps = "Step 1: Provide the target URL or IP address to scan.\nStep 2: Select the scan type (TCP or UDP).\nStep 3: Configure additional scan options as needed.\nStep 4: Start the scan to gather information about potential vulnerabilities and misconfigurations.\nStep 5: Review the scan output to identify any security issues.\n";
+    const steps =
+        "Step 1: Provide the target URL or IP address to scan.\nStep 2: Select the scan type (TCP or UDP).\nStep 3: Configure additional scan options as needed.\nStep 4: Start the scan to gather information about potential vulnerabilities and misconfigurations.\nStep 5: Review the scan output to identify any security issues.\n";
     const sourceLink = "https://github.com/dneufeld/unicornscan";
     const tutorial = "";
     const dependencies = ["unicornscan"];
@@ -58,24 +59,36 @@ const Unicornscan = () => {
             targetIP: "",
         },
     });
-
+    // Check if the command is available and set the state variables accordingly.
     useEffect(() => {
+        // Check if the command is available and set the state variables accordingly.
         checkAllCommandsAvailability(dependencies)
             .then((isAvailable) => {
-                setIsCommandAvailable(isAvailable);
-                setOpened(!isAvailable);
-                setLoadingModal(false);
+                setIsCommandAvailable(isAvailable); // Set the command availability state
+                setOpened(!isAvailable); // Set the modal state to opened if the command is not available
+                setLoadingModal(false); // Set loading to false after the check is done
             })
             .catch((error) => {
                 console.error("An error occurred:", error);
-                setLoadingModal(false);
+                setLoadingModal(false); // Also set loading to false in case of error
             });
     }, []);
-
+    /**
+     * handleProcessData: Callback to handle and append new data from the child process to the output.
+     * It updates the state by appending the new data received to the existing output.
+     * @param {string} data - The data received from the child process.
+     */
     const handleProcessData = useCallback((data: string) => {
         setOutput((prevOutput) => prevOutput + "\n" + data);
     }, []);
-
+    /**
+     * handleProcessTermination: Callback to handle the termination of the child process.
+     * Once the process termination is handled, it clears the process PID reference and
+     * deactivates the loading overlay.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
+     */
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
             if (code === 0) {
@@ -90,7 +103,13 @@ const Unicornscan = () => {
         },
         [handleProcessData]
     );
-
+    /**
+     * onSubmit: Asynchronous handler for the form submission event.
+     * It sets up and triggers the tshark tool with the given parameters.
+     * Once the command is executed, the results or errors are displayed in the output.
+     *
+     * @param {FormValuesType} values - The form values, containing the <list the form values here, e.g.  interface, packet count, etc>.
+     */
     const onSubmit = async (values: FormValuesType) => {
         setLoading(true);
         let args = [values.targetIP];
@@ -110,12 +129,7 @@ const Unicornscan = () => {
         if (ports) args.push(`-p ${ports}`);
         if (verboseMode) args.push("-v");
 
-        CommandHelper.runCommandWithPkexec(
-            "unicornscan",
-            [...args, "-I"],
-            handleProcessData,
-            handleProcessTermination
-        )
+        CommandHelper.runCommandWithPkexec("unicornscan", [...args, "-I"], handleProcessData, handleProcessTermination)
             .then(() => {
                 setLoading(false);
             })
@@ -126,11 +140,17 @@ const Unicornscan = () => {
         setAllowSave(true);
     };
 
+    /**
+     * Handles the completion of output saving by updating state variables.
+     */
     const handleSaveComplete = () => {
         setHasSaved(true);
         setAllowSave(false);
     };
 
+    /**
+     * Clears the command output and resets state variables related to output saving.
+     */
     const clearOutput = () => {
         setOutput("");
         setHasSaved(false);
@@ -157,7 +177,12 @@ const Unicornscan = () => {
             <form onSubmit={form.onSubmit((values) => onSubmit({ ...values, scanType: selectedScanType }))}>
                 <Stack>
                     {LoadingOverlayAndCancelButton(loading, pid)}
-                    <TextInput label="IP Address" required {...form.getInputProps("targetIP")} placeholder="e.g., 192.168.102" />
+                    <TextInput
+                        label="IP Address"
+                        required
+                        {...form.getInputProps("targetIP")}
+                        placeholder="e.g., 192.168.102"
+                    />
                     <NativeSelect
                         label="Scan Type"
                         value={selectedScanType}
