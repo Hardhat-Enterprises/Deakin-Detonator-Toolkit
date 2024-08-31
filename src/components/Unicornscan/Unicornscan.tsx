98ab4a0909a398049c41e3782a6ef686a520fee3
SethJ1 <sethj4774@gmail.com>
2024-08-31 14:24:35 +1000

    added functionality and restructured code

--- src/components/Unicornscan/Unicornscan.tsx
+++ src/components/Unicornscan/Unicornscan.tsx
@@ -1,72 +1,145 @@
-import { Button, NativeSelect, Stack, TextInput } from "@mantine/core";
+import { useState, useEffect, useCallback } from "react";
+import { Button, Stack, TextInput, Checkbox } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useState } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { RenderComponent } from "../UserGuide/UserGuide";
+import InstallationModal from "../InstallationModal/InstallationModal";
+import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
+import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
 
-// Component Constants.
-const title = "Unicornscan"; // Title of the component.
-const description = "Unicornscan is a tool used to used to gather information about systems and services on a network"; // Description of the component.
-const steps =
-    "Step 1: Provide the target URL or IP address to scan.\n" +
-    "Step 2: Start the scan to gather information about potential vulnerabilities and misconfigurations.\n" +
-    "Step 3: Review the scan output to identify any security issues.\n";
-const sourceLink = "https://github.com/dneufeld/unicornscan"; // Link to the source code (or Kali Tools).
-const tutorial = ""; // Link to the official documentation/tutorial.
-const dependencies = ["unicornscan"]; // Contains the dependencies required by the component.
-
-//Variables
-interface FormValuesType { //Relevant form values to be added as Unicornscan options are added
-    unicornscanOptions: string; 
+/**
+ * Represents the form values for the Unicornscan component.
+ */
+interface FormValuesType { 
+    targetIP: string;
 }
 
-//Unicornscan Options
-const unicornscanOptions = [ //More options to be added (version check could be kept?)
-    "Version Check"
-];
-
+/**
+ * The Unicornscan component.
+ * @returns The Unicornscan component.
+ */
 const Unicornscan = () => {
-    var [output, setOutput] = useState("");
-    const [selectedUnicornscanOption, setSelectedUnicornscanOption] = useState("");
+    // Component state variables
+    const [loading, setLoading] = useState(false); // State variable to indicate loading state
+    const [output, setOutput] = useState(""); // State variable to store the output of the command execution
     const [allowSave, setAllowSave] = useState(false); // State variable to allow saving of output
     const [hasSaved, setHasSaved] = useState(false); // State variable to indicate if output has been saved
-    const [loading, setLoading] = useState(false); // State variable to indicate loading state
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
+    const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
+    const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
+    const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
+    const [verboseMode, setVerboseMode] = useState(false); // State variable for verbose mode
 
-    let form = useForm({ //Relevant form values to be added as Unicornscan options are added
+    // Component Constants
+    const title = "Unicornscan"; // Title of the component.
+    const description = "Unicornscan is a tool used to used to gather information about systems and services on a network"; // Description of the component.
+    const steps =
+        "Step 1: Provide the target URL or IP address to scan.\n" +
+        "Step 2: Start the scan to gather information about potential vulnerabilities and misconfigurations.\n" +
+        "Step 3: Review the scan output to identify any security issues.\n";
+    const sourceLink = "https://github.com/dneufeld/unicornscan"; // Link to the source code (or Kali Tools).
+    const tutorial = ""; // Link to the official documentation/tutorial.
+    const dependencies = ["unicornscan"]; // Contains the dependencies required by the component.
+    
+    // Form hook to handle form input
+    let form = useForm({
         initialValues: {
-            input1: "",
+            targetIP: "",
         },
     });
 
+    useEffect(() => {
+        // Check if the command is available and set the state variables accordingly.
+        checkAllCommandsAvailability(dependencies)
+            .then((isAvailable) => {
+                setIsCommandAvailable(isAvailable); // Set the command availability state
+                setOpened(!isAvailable); // Set the modal state to opened if the command is not available
+                setLoadingModal(false); // Set loading to false after the check is done
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
+                setLoadingModal(false); // Also set loading to false in case of error
+            });
+    }, []);
+
+    /**
+     * handleProcessData: Callback to handle and append new data from the child process to the output.
+     * It updates the state by appending the new data received to the existing output.
+     * @param {string} data - The data received from the child process.
+     */
+    const handleProcessData = useCallback((data: string) => {
+        setOutput((prevOutput) => prevOutput + "\n" + data); // Append new data to the previous output.
+    }, []);
+
+    /**
+     * handleProcessTermination: Callback to handle the termination of the child process.
+     * Once the process termination is handled, it clears the process PID reference and
+     * deactivates the loading overlay.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
+     */
+    const handleProcessTermination = useCallback(
+        ({ code, signal }: { code: number; signal: number }) => {
+            // If the process was successful, display a success message.
+            if (code === 0) {
+                handleProcessData("\nProcess completed successfully.");
+
+                // If the process was terminated manually, display a termination message.
+            } else if (signal === 15) {
+                handleProcessData("\nProcess was manually terminated.");
+
+                // If the process was terminated with an error, display the exit and signal codes.
+            } else {
+                handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
+            }
+
+            // Clear the child process pid reference. There is no longer a valid process running.
+            setPid("");
+
+            // Cancel the loading overlay. The process has completed.
+            setLoading(false);
+        },
+        [handleProcessData] // Dependency on the handleProcessData callback
+    );
+
+    /**
+     * Handles form submission for the Masscan component.
+     * @param {FormValuesType} values - The form values containing the target domain.
+     */
     const onSubmit = async (values: FormValuesType) => {
-        // Set loading state to true and disallow output saving
+        // Activate loading state to indicate ongoing process
         setLoading(true);
-        setAllowSave(false);
 
-        let args = [``];
+        // Construct arguments for the Masscan command based on form input
+        let args = [];
+        args = [values.targetIP];
 
-        //Switch case
-        switch (values.unicornscanOptions) {
-            case "Version Check": //This version check option is a baseline to test that Unicornscan functions
-                args = [`-V`];
-
-                try {
-                    let output = await CommandHelper.runCommand("unicornscan", args);
-                    setOutput(output);
-                } catch (e: any) {
-                    setOutput(e);
-                } finally{
-                    // Set loading state to false and allow output saving
-                    setLoading(false);
-                    setAllowSave(true);
-                }
-
-                break;
+        if (verboseMode) {
+            args.push("-v"); // Add verbose mode option if enabled
         }
-    };
 
+        // Execute the Masscan command via helper method and handle its output or potential errors
+        CommandHelper.runCommandWithPkexec(
+            "unicornscan",
+            [...args, "-I"],
+            handleProcessData,
+            handleProcessTermination
+        )
+            .then(() => {
+                // Deactivate loading state
+                setLoading(false);
+            })
+            .catch((error) => {
+                // Display any errors encountered during command execution
+                setOutput(`Error: ${error.message}`);
+                // Deactivate loading state
+                setLoading(false);
+            });
+            setAllowSave(true);
+    };
      /**
      * Handles the completion of output saving by updating state variables.
      */
@@ -84,7 +157,7 @@ const Unicornscan = () => {
         setAllowSave(false); // Disallow further output saving
     };
 
-    //<ConsoleWrapper output={output} clearOutputCallback={clearOutput} /> prints the terminal on the tool
+    // Render component
     return (
         <RenderComponent
             title={title}
@@ -93,24 +166,28 @@ const Unicornscan = () => {
             tutorial={tutorial}
             sourceLink={sourceLink}
         >
-        <form onSubmit={form.onSubmit((values) => onSubmit({ ...values, unicornscanOptions: selectedUnicornscanOption }))}>
-            <Stack>
-                
-                <NativeSelect
-                    value={selectedUnicornscanOption}
-                    onChange={(e) => setSelectedUnicornscanOption(e.target.value)}
-                    title={"Unicornscan option"}
-                    data={unicornscanOptions}
-                    required
-                    placeholder={"Pick a Unicornscan option"}
-                    description={"Type of action to perform"}
+            {!loadingModal && (
+                <InstallationModal
+                    isOpen={opened}
+                    setOpened={setOpened}
+                    feature_description={description}
+                    dependencies={dependencies}
                 />
-                <TextInput label={"Input 1"} {...form.getInputProps("input1")} />
-                <Button type={"submit"}>start unicornscan</Button>
-                {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
-                <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
-            </Stack>
-        </form>
+            )}
+            <form onSubmit={form.onSubmit(onSubmit)}>
+                <Stack>
+                    {LoadingOverlayAndCancelButton(loading, pid)}
+                    <TextInput label="IP Address" required {...form.getInputProps("targetIP")} />
+                    <Checkbox
+                        label="Verbose Mode"
+                        checked={verboseMode}
+                        onChange={(event) => setVerboseMode(event.currentTarget.checked)}
+                    />
+                    <Button type={"submit"}>Start {title}</Button>
+                    {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                    <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+                </Stack>
+            </form>
         </RenderComponent>
     );
 };
