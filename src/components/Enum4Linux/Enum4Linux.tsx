8a06ba3b64797ecd18caff6638be685d06b2215e
rng4302 <rajgariwala40302@gmail.com>
2024-09-11 01:29:39 -0400

    Changing style and adding few required elements

--- src/components/Enum4Linux/Enum4Linux.tsx
+++ src/components/Enum4Linux/Enum4Linux.tsx
@@ -8,6 +8,7 @@ import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFil
 import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
 import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
 import InstallationModal from "../InstallationModal/InstallationModal";
+import React from "react";
 
 /**
  * Represents the form values for the Enum4Linux component.
@@ -37,6 +38,12 @@ const Enum4Linux = () => {
     const dependencies = ["enum4linux"];
     const title = "Enum4Linux";
     const description = "Enum4linux is a tool used for enumerating information from Windows and Samba systems.";
+    const steps =
+        "Step 1: Enter the Ip address of the target.\n" +
+        "Step 2: Type in the option you want to do enumeration.\n" +
+        "Step 3: Enter any additional parameters you want to add.";
+    const sourceLink = ""; // Link to the source code (or Kali Tools).
+    const tutorial = ""; // Link to the official documentation/tutorial.
 
     const form = useForm({
         initialValues: {
@@ -62,30 +69,33 @@ const Enum4Linux = () => {
     }, []);
 
     const handleProcessData = useCallback((data: string) => {
-        setOutput(prevOutput => prevOutput + "\n" + data);
+        setOutput((prevOutput) => prevOutput + "\n" + data);
     }, []);
 
-    const handleProcessTermination = useCallback(({ code, signal }) => {
-        let message = `Process terminated with exit code: ${code} and signal code: ${signal}`;
-        if (code === 0) {
-            message = "\nProcess completed successfully.";
-        } else if (signal === 15) {
-            message = "\nProcess was manually terminated.";
-        }
-        handleProcessData(message);
-        setPid("");
-        setLoading(false);
-        setAllowSave(true);
-        setHasSaved(false);
-    }, [handleProcessData]);
+    const handleProcessTermination = useCallback(
+        ({ code, signal }: { code: number; signal: number }) => {
+            let message = `Process terminated with exit code: ${code} and signal code: ${signal}`;
+            if (code === 0) {
+                message = "\nProcess completed successfully.";
+            } else if (signal === 15) {
+                message = "\nProcess was manually terminated.";
+            }
+            handleProcessData(message);
+            setPid("");
+            setLoading(false);
+            setAllowSave(true);
+            setHasSaved(false);
+        },
+        [handleProcessData]
+    );
 
     const onSubmit = async (values: FormValuesType) => {
         setLoading(true);
         setAllowSave(false);
         const options = `-${values.argumentMain}`;
-        const args = values.argumentAlt ? 
-                     [options, values.paramMain, `-${values.argumentAlt}`, values.paramAlt, values.ipAddress] : 
-                     [options, values.paramMain, values.ipAddress];
+        const args = values.argumentAlt
+            ? [options, values.paramMain, `-${values.argumentAlt}`, values.paramAlt, values.ipAddress]
+            : [options, values.paramMain, values.ipAddress];
 
         CommandHelper.runCommandGetPidAndOutput("enum4linux", args, handleProcessData, handleProcessTermination)
             .then(({ pid, output }) => {
@@ -112,7 +122,13 @@ const Enum4Linux = () => {
     };
 
     return (
-        <RenderComponent title={title} description={description}>
+        <RenderComponent
+            title={title}
+            description={description}
+            steps={steps}
+            tutorial={tutorial}
+            sourceLink={sourceLink}
+        >
             {!loadingModal && (
                 <InstallationModal
                     isOpen={opened}
