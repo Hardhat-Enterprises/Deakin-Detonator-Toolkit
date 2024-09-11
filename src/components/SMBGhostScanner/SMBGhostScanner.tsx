fb12457e6b659d59fa90d96fadde3df1513768ad
Nirodha Kehelpannala <167403681+s223915833@users.noreply.github.com>
2024-09-10 23:37:35 -0400

    CI fix and File name change to SMBGhostScanner

---
+++ src/components/SMBGhostScanner/SMBGhostScanner.tsx
@@ -0,0 +1,201 @@
+import { Button, Stack, TextInput, Alert } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useCallback, useState, useEffect } from "react";
+import { CommandHelper } from "../../utils/CommandHelper";
+import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
+import { IconAlertCircle } from "@tabler/icons";
+import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
+import { RenderComponent } from "../UserGuide/UserGuide";
+import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
+import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/OverlayAndCancelButton";
+import InstallationModal from "../InstallationModal/InstallationModal";
+
+const title = "SMB-Ghost Scanner"; // Title of the component.
+const description =
+    "SMB-Ghost Scanner is a tool used to scan a target for vulnerability to the CVE2020-0796 attack vector."; // Description of the component.
+const steps =
+    "Step 1: Enter a Target IP address.\n" +
+    "Step 2: Click scan to commence SMB-Ghost Scanners operation.\n" +
+    "Step 3: View the Output block below to view the results of the tools execution."; // Steps for viewing the component
+const sourceLink = ""; // Link to the source code or documentation.
+const tutorial = ""; // Link to the official documentation/tutorial.
+const dependencies = ["python3"]; // Contains the dependencies required by the component.
+/**
+ * Interface representing the form values used in the SMBGhostScanner component.
+ */
+interface FormValuesType {
+    ip: string;
+}
+/**
+ * The SMBGhostScanner component.
+ * @returns The SMBGhostScanner component.
+ */
+const SMBGhostScanner = () => {
+    // Component State Variables
+    const [loading, setLoading] = useState(false); // State variable to indicate loading state.
+    const [output, setOutput] = useState(""); // State variable to store the output of the command execution.
+    const [allowSave, setAllowSave] = useState(false); //   State variable to allow saving the output to a file.
+    const [hasSaved, setHasSaved] = useState(false); // State variable to indicate if the output has been saved.
+    const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
+    const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
+    const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
+
+    // form hook to handle form input
+    let form = useForm({
+        initialValues: {
+            ip: "",
+        },
+        validate: {
+            ip: (value) => (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(value) ? null : "Invalid IP address"),
+        },
+    });
+
+    // Check if the command is available and set the state variables accordingly.
+    useEffect(() => {
+        // Check if the command is available and set the state variables accordingly.
+        checkAllCommandsAvailability(dependencies)
+            .then((isAvailable) => {
+                setIsCommandAvailable(isAvailable); // Set the command availability state
+                setOpened(!isAvailable); // Set the modal state to opened if the command is not available
+                setLoadingModal(false); // Set loading to false after the check is done
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
+                setLoadingModal(false); // Also set loading to false in case of error
+            });
+    }, []);
+
+    // Process Handlers
+    /**
+     * Callback to handle and append new data from the child process to the output.
+     * It updates the state by appending the new data received to the existing output.
+     * @param {string} data - The data received from the child process.
+     */
+    const handleProcessData = useCallback((data: string) => {
+        setOutput((prevOutput) => prevOutput + "\n" + data); // Update output
+    }, []);
+
+    /**
+     * Callback to handle the termination of the child process.
+     * Once the process termination is handled, it clears the process PID reference and
+     * deactivates the loading overlay.
+     * @param {object} param - An object containing information about the process termination.
+     * @param {number} param.code - The exit code of the terminated process.
+     * @param {number} param.signal - The signal code indicating how the process was terminated.
+     */
+    const handleProcessTermination = useCallback(
+        ({ code, signal }: { code: number; signal: number }) => {
+            if (code === 0) {
+                handleProcessData("\nProcess completed successfully.");
+            } else if (signal === 15) {
+                handleProcessData("\nProcess was manually terminated.");
+            } else {
+                handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
+            }
+            // Clear the child process pid reference
+            setPid("");
+            // Cancel the Loading Overlay
+            setLoading(false);
+            // Allow Saving as the output is finalised
+            setAllowSave(true);
+            setHasSaved(false);
+        },
+        [handleProcessData]
+    );
+
+    /**
+     * Actions taken after saving the output.
+     * It updates the state to reflect that the output has been saved.
+     */
+    const handleSaveComplete = () => {
+        // Indicating that the file has saved which is passed
+        // back into SaveOutputToTextFile to inform the user
+        setHasSaved(true);
+        setAllowSave(false);
+    };
+
+    /**
+     * Asynchronous handler for the form submission event.
+     * It sets up and triggers the SMB-Ghost Scanner tool with the given parameters.
+     * Once the command is executed, the results or errors are displayed in the output.
+     * @param {FormValuesType} values - The form values, containing the target IP address.
+     */
+    const onSubmit = async (values: FormValuesType) => {
+        // Validate IP address to command injection prevention
+        if (!/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(values.ip)) {
+            setOutput("Invalid IP address format.");
+            setLoading(false);
+            return;
+        }
+
+        // Disallow saving until the tool's execution is complete
+        setAllowSave(false);
+        // Start the Loading Overlay
+        setLoading(true);
+
+        const args = [`/usr/share/ddt/SMBGhostScanner.py`, values.ip];
+
+        try {
+            const result = await CommandHelper.runCommandGetPidAndOutput(
+                "python3",
+                args,
+                handleProcessData,
+                handleProcessTermination
+            );
+            setPid(result.pid);
+            setOutput(result.output);
+        } catch (e: any) {
+            setOutput(e.message);
+        }
+    };
+
+    /**
+     * Clears the output state and resets save state variables to defaults.
+     */
+    const clearOutput = useCallback(() => {
+        setOutput("");
+
+        // reset save state variables to defaults
+        setHasSaved(false);
+        setAllowSave(false);
+    }, [setOutput]);
+
+    return (
+        <RenderComponent
+            title={title}
+            description={description}
+            steps={steps}
+            tutorial={tutorial}
+            sourceLink={sourceLink}
+        >
+            {!loadingModal && (
+                <InstallationModal
+                    isOpen={opened}
+                    setOpened={setOpened}
+                    feature_description={description}
+                    dependencies={dependencies}
+                ></InstallationModal>
+            )}
+
+            <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
+                <Stack>
+                    {LoadingOverlayAndCancelButton(loading, pid)}
+                    <Alert
+                        icon={<IconAlertCircle size={16} />}
+                        radius="md"
+                        children={
+                            "Please turn off the firewall on target system, otherwise the detect packet might be dropped. "
+                        }
+                    ></Alert>
+                    <TextInput label={"Target IP address"} required {...form.getInputProps("ip")} />
+                    <Button type={"submit"}>Scan</Button>
+                    {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
+                    <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
+                </Stack>
+            </form>
+        </RenderComponent>
+    );
+};
+
+export default SMBGhostScanner;
