4404458081408743c320918ad79de522ba88f45c
Tillu6 <sakethreddy018@gmail.com>
2024-09-15 14:31:45 -0400

    photon tool

--- src/components/RouteWrapper.tsx
+++ src/components/RouteWrapper.tsx
@@ -583,6 +583,13 @@ export const ROUTES: RouteProperties[] = [
         category: "Web Application Testing",
     },
     {
+        name: "Photon",
+        path: "/tools/photon",
+        element: <Photon />,
+        description: "A fast and flexible crawler designed for open source intelligence.",
+        category: "Information Gathering and Analysis",
+    },
+    {
         name: "RTsort",
         path: "/tools/RTsort",
         element: <RTsort />,
