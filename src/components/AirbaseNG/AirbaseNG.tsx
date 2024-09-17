77720877d6c9c7e548aa246166c344a0e533a901
shivang2311 <139949843+shivang2311@users.noreply.github.com>
2024-09-18 09:16:19 +1000

    513-feature (#901)
    * Update AirbaseNG.tsx
    
    The new code introduces significant improvements for code standardization. It adds Advanced and Custom modes, allowing for more flexibility in configuration. The Advanced mode includes additional options such as verbosity and disabling beacons, while the Custom mode supports custom scripts. The interface is updated to include these new options, and error handling has been enhanced. Additionally, the layout has been improved with better organization, tooltips, and command previews, making the code more user-friendly and adhering to the style guide for readability and maintainability.
    
    * made changes as told
    
    * Update style
    
    * Update style-2
    
    * made changes as told 
    
    Add all original comments and check tooltips
    
    * Update style-2
    
    * Update style-3
    
    * Update style
    
    * made changes in comments
    
    ---------
    
    Co-authored-by: michaeljpigott <35479648+michaeljpigott@users.noreply.github.com>

--- src/components/AirbaseNG/AirbaseNG.tsx
+++ src/components/AirbaseNG/AirbaseNG.tsx
@@ -1,16 +1,17 @@
-import { Button, Stack, Switch, TextInput } from "@mantine/core";
+import { Button, Stack, Switch, TextInput, Tooltip } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useCallback, useState, useEffect } from "react";
 import { CommandHelper } from "../../utils/CommandHelper";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { RenderComponent } from "../UserGuide/UserGuide";
-import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile"; //v2
+import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile";
 import { LoadingOverlayAndCancelButtonPkexec } from "../OverlayAndCancelButton/OverlayAndCancelButton";
 import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
 import InstallationModal from "../InstallationModal/InstallationModal";
 
 /**
  * Represents the form values for the AirbaseNG component.
+ * @interface
  */
 interface FormValuesType {
     fakeHost: string;
@@ -18,15 +19,19 @@ interface FormValuesType {
     macAddress: string;
     replayInterface: string;
     filePath: string;
+    verbosity: boolean; // Advanced mode: Verbosity flag
+    disableBeacons: boolean; // Advanced mode: Disable beacons
     customConfig: string;
+    customScript: string; // Custom mode: Custom script path
 }
 
 /**
- * The AirbaseNG component.
+ * The AirbaseNG component handles the setup and execution of the airbase-ng tool.
+ * Supports Basic, Advanced, and Custom modes for flexibility in input parameters.
  * @returns The AirbaseNG component.
  */
 const AirbaseNG = () => {
-    // Component State Variables.
+    // State variables to manage the component's functionality.
     const [loading, setLoading] = useState(false); // State variable to indicate loading state.
     const [output, setOutput] = useState(""); // State variable to store the output of the command execution.
     const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
@@ -38,7 +43,7 @@ const AirbaseNG = () => {
     const [advanceMode, setAdvanceMode] = useState(false);
     const [customMode, setCustomMode] = useState(false);
 
-    // Component Constants.
+    // Constants for the component's metadata.
     const title = "Airbase-ng"; // Title of the component.
     const description = "Airbase-ng is a tool to create fake access points."; // Description of the component.
     const steps =
@@ -46,24 +51,27 @@ const AirbaseNG = () => {
         "Step 2: Select your desired channel.\n" +
         "Step 3: Specify the WLAN interface to be used.\n" +
         "Step 4: Click 'Start AP' to begin the process.\n" +
-        "Step 5: View the output block to see the results. ";
+        "Step 5: View the output block to see the results.";
     const sourceLink = "https://www.kali.org/tools/aircrack-ng/#airbase-ng"; // Link to the source code (or Kali Tools).
     const tutorial = ""; // Link to the official documentation/tutorial.
-    const dependencies = ["aircrack-ng"]; // Contains the dependencies required by the component.
+    const dependencies = ["aircrack-ng"]; // Contains the dependencies required by the compon
 
-    // Form hook to handle form input.
+    // Form handling for the component's input fields.
     const form = useForm({
         initialValues: {
             fakeHost: "",
-            channel: "",
-            replayInterface: "",
+            channel: "6", // Default channel
+            replayInterface: "wlan0", // Default interface
             macAddress: "",
             filePath: "",
+            verbosity: false,
+            disableBeacons: false,
             customConfig: "",
+            customScript: "",
         },
     });
 
-    // Check if the command is available and set the state variables accordingly.
+    // Effect to check command availability on component mount.
     useEffect(() => {
         // Check if the command is available and set the state variables accordingly.
         checkAllCommandsAvailability(dependencies)
@@ -72,7 +80,8 @@ const AirbaseNG = () => {
                 setOpened(!isAvailable); // Set the modal state to opened if the command is not available
                 setLoadingModal(false); // Set loading to false after the check is done
             })
-            .catch((error) => {
+            .catch((error: any) => {
+                // Fix the error type
                 console.error("An error occurred:", error);
                 setLoadingModal(false); // Also set loading to false in case of error
             });
@@ -97,27 +106,22 @@ const AirbaseNG = () => {
      */
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
-            // If the process was successful, display a success message.
             if (code === 0) {
+                // If the process was successful, display a success message.
                 handleProcessData("\nProcess completed successfully.");
-
-                // If the process was terminated manually, display a termination message.
             } else if (signal === 15) {
+                // If the process was terminated manually, display a termination message.
                 handleProcessData("\nProcess was manually terminated.");
-
-                // If the process was terminated with an error, display the exit and signal codes.
             } else {
+                // If the process was terminated with an error, display the exit and signal codes.
                 handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
             }
-
-            // Clear the child process pid reference. There is no longer a valid process running.
             setPid("");
-
-            // Cancel the loading overlay. The process has completed.
+            // Clear the child process pid reference. There is no longer a valid process running.
             setLoading(false);
-
-            // Allow Saving as the output is finalised
+            // Cancel the loading overlay. The process has completed.
             setAllowSave(true);
+            // Allow Saving as the output is finalised
             setHasSaved(false);
         },
         [handleProcessData] // Dependency on the handleProcessData callback
@@ -131,35 +135,46 @@ const AirbaseNG = () => {
      * @param {FormValuesType} values - The form values, containing the fake host name, channel, and WLAN interface.
      */
     const onSubmit = async (values: FormValuesType) => {
-        // Activate loading state to indicate ongoing process
         setLoading(true);
-
-        // Disallow saving until the tool's execution is complete
+        // Activate loading state to indicate ongoing process
         setAllowSave(false);
+        // Disallow saving until the tool's execution is complete
 
-        // Construct arguments for the aircrack-ng command based on form input
+        // Construct arguments for the airbase-ng command based on form input
         const args = ["-e", values.fakeHost, "-c", values.channel, values.replayInterface];
 
-        values.macAddress ? args.push(`-a`, values.macAddress) : undefined;
-        values.filePath ? args.push(`-F`, values.filePath) : undefined;
-        values.customConfig ? args.push(values.customConfig) : undefined;
+        // Advanced Mode Options
+        if (advanceMode) {
+            if (values.verbosity) args.push("-v");
+            if (values.disableBeacons) args.push("--no-beacons");
+        }
 
-        // Execute the aircrack-ng command via helper method and handle its output or potential errors
-        CommandHelper.runCommandWithPkexec("airbase-ng", args, handleProcessData, handleProcessTermination)
-            .then(({ output, pid }) => {
-                // Update the UI with the results from the executed command
-                setOutput(output);
-                setAllowSave(true);
-                console.log(pid);
-                setPid(pid);
-            })
-            .catch((error) => {
-                // Display any errors encountered during command execution
-                setOutput(error.message);
-                // Deactivate loading state
-                setLoading(false);
-                setAllowSave(true);
-            });
+        // Additional parameters for Advanced and Custom modes
+        if (values.macAddress) args.push("-a", values.macAddress);
+        if (values.filePath) args.push("-F", values.filePath);
+        if (values.customConfig) args.push(values.customConfig);
+
+        // Custom Mode Option: Custom script execution
+        if (customMode && values.customScript) {
+            args.push(`&& bash ${values.customScript}`);
+        }
+
+        try {
+            const { output, pid } = await CommandHelper.runCommandWithPkexec(
+                "airbase-ng",
+                args,
+                handleProcessData,
+                handleProcessTermination
+            );
+            setOutput(output);
+            setAllowSave(true);
+            setPid(pid);
+        } catch (error: any) {
+            // Display any errors encountered during command execution
+            setOutput(error.message || "An unknown error occurred.");
+            setLoading(false); // Deactivate loading state
+            setAllowSave(true);
+        }
     };
 
     /**
@@ -172,9 +187,8 @@ const AirbaseNG = () => {
     }, [setOutput]);
 
     const handleSaveComplete = () => {
-        // Indicating that the file has saved which is passed
+        setHasSaved(true); // Indicating that the file has saved which is passed
         // back into SaveOutputToTextFile to inform the user
-        setHasSaved(true);
         setAllowSave(false);
     };
 
@@ -192,38 +206,78 @@ const AirbaseNG = () => {
                     setOpened={setOpened}
                     feature_description={description}
                     dependencies={dependencies}
-                ></InstallationModal>
+                />
             )}
             <form onSubmit={form.onSubmit(onSubmit)}>
                 <Stack>
                     {LoadingOverlayAndCancelButtonPkexec(loading, pid, handleProcessData, handleProcessTermination)}
-                    <Switch
-                        size="md"
-                        label="Advanced Mode"
-                        checked={advanceMode}
-                        onChange={(e) => setAdvanceMode(e.currentTarget.checked)}
-                    />
-                    <Switch
-                        size="md"
-                        label="Custom Configuration"
-                        checked={customMode}
-                        onChange={(e) => setCustomMode(e.currentTarget.checked)}
-                    />
-                    <TextInput label={"Name of your fake host"} required {...form.getInputProps("fakeHost")} />
-                    <TextInput label={"Channel of choice"} required {...form.getInputProps("channel")} />
-                    <TextInput label={"Your WLAN interface"} required {...form.getInputProps("replayInterface")} />
+
+                    {/* Advanced Mode Switch */}
+                    <Tooltip label="Enable advanced settings">
+                        <Switch
+                            size="md"
+                            label="Advanced Mode"
+                            checked={advanceMode}
+                            onChange={(e) => setAdvanceMode(e.currentTarget.checked)}
+                        />
+                    </Tooltip>
+
+                    {/* Custom Mode Switch */}
+                    <Tooltip label="Enable custom configuration">
+                        <Switch
+                            size="md"
+                            label="Custom Configuration"
+                            checked={customMode}
+                            onChange={(e) => setCustomMode(e.currentTarget.checked)}
+                        />
+                    </Tooltip>
+
+                    {/* Basic Mode Inputs */}
+                    <TextInput label="Name of your fake host" required {...form.getInputProps("fakeHost")} />
+                    <TextInput label="Channel of choice" required {...form.getInputProps("channel")} />
+                    <TextInput label="Your WLAN interface" required {...form.getInputProps("replayInterface")} />
+
+                    {/* Advanced Mode Inputs */}
                     {advanceMode && (
                         <>
-                            <TextInput label={"Set AP MAC address"} {...form.getInputProps("MACAddress")} />
+                            <TextInput label="Set AP MAC address" {...form.getInputProps("macAddress")} />
                             <TextInput
-                                label={"Save as Pcap File (Please Supply FilePath)"}
+                                label="Save as Pcap File (Please Supply FilePath)"
                                 {...form.getInputProps("filePath")}
                             />
+                            <Tooltip label="Enable verbose output">
+                                <Switch
+                                    label="Verbose output"
+                                    {...form.getInputProps("verbosity", { type: "checkbox" })}
+                                />
+                            </Tooltip>
+                            <Tooltip label="Disable sending beacons">
+                                <Switch
+                                    label="Disable beacons"
+                                    {...form.getInputProps("disableBeacons", { type: "checkbox" })}
+                                />
+                            </Tooltip>
                         </>
                     )}
-                    {customMode && <TextInput label={"Custom Configuration"} {...form.getInputProps("customConfig")} />}
+
+                    {/* Custom Mode Inputs */}
+                    {customMode && (
+                        <>
+                            <TextInput label="Custom configuration" {...form.getInputProps("customConfig")} />
+                            <TextInput
+                                label="Custom script path"
+                                placeholder="Path to custom script"
+                                {...form.getInputProps("customScript")}
+                            />
+                            <div>
+                                <strong>Command Preview:</strong> airbase-ng {form.values.fakeHost}{" "}
+                                {form.values.customConfig}
+                            </div>
+                        </>
+                    )}
+
                     {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
-                    <Button type={"submit"}>Start {title}</Button>
+                    <Button type="submit">Start {title}</Button>
                     <ConsoleWrapper output={output} clearOutputCallback={clearOutput} />
                 </Stack>
             </form>
