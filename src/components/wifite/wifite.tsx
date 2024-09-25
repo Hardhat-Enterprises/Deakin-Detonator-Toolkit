de381af9b180062d35d45b3127b44a165539645a
Bipan <162571104+Bipan079@users.noreply.github.com>
2024-09-25 11:11:24 +1000

    Update wifite.tsx

--- src/components/wifite/wifite.tsx
+++ src/components/wifite/wifite.tsx
@@ -9,31 +9,22 @@ import { LoadingOverlayAndCancelButton } from "../OverlayAndCancelButton/Overlay
 import { checkAllCommandsAvailability } from "../../utils/CommandAvailability";
 import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFile"; //v2
 
-/**
- * Represents the form values for the Wifite component.
- */
 interface FormValuesType {
     targetInterface: string;
     attackMode: string;
     channel: string;
 }
 
-/**
- * The Wifite component.
- * @returns The Wifite component.
- */
 const Wifite = () => {
-    // Component state variables
-    const [loading, setLoading] = useState(false); // State variable to indicate loading state
-    const [output, setOutput] = useState(""); // State variable to store the output of the command execution
-    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
-    const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
-    const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
-    const [pid, setPid] = useState(""); // State variable to store the process ID of the command execution.
-    const [verboseMode, setVerboseMode] = useState(false); // State variable for verbose mode
-    const [allowSave, setAllowSave] = useState(false); // State to allow saving output
+    const [loading, setLoading] = useState(false);
+    const [output, setOutput] = useState("");
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false);
+    const [opened, setOpened] = useState(!isCommandAvailable);
+    const [loadingModal, setLoadingModal] = useState(true);
+    const [pid, setPid] = useState("");
+    const [verboseMode, setVerboseMode] = useState(false);
+    const [allowSave, setAllowSave] = useState(false);
 
-    // Component Constants
     const title = "Wifite";
     const description =
         "Wifite is a Python tool for automating the process of attacking WPA and WPA2 networks. It handles the cracking of handshakes and supports multiple attack modes.";
@@ -45,11 +36,10 @@ const Wifite = () => {
         " \n" +
         "=== Optional ===\n" +
         "Step 4: Enable verbose mode for more detailed output.\n";
-    const sourceLink = ""; // Link to the source code
-    const tutorial = ""; // Link to the official documentation/tutorial
-    const dependencies = ["wifite"]; // Contains the dependencies required by the component.
+    const sourceLink = ""; 
+    const tutorial = ""; 
+    const dependencies = ["wifite"];
 
-    // Form hook to handle form input
     let form = useForm({
         initialValues: {
             targetInterface: "",
@@ -59,99 +49,63 @@ const Wifite = () => {
     });
 
     useEffect(() => {
-        // Check if the command is available and set the state variables accordingly.
         checkAllCommandsAvailability(dependencies)
             .then((isAvailable) => {
-                setIsCommandAvailable(isAvailable); // Set the command availability state
-                setOpened(!isAvailable); // Set the modal state to opened if the command is not available
-                setLoadingModal(false); // Set loading to false after the check is done
+                setIsCommandAvailable(isAvailable);
+                setOpened(!isAvailable);
+                setLoadingModal(false);
             })
             .catch((error) => {
                 console.error("An error occurred:", error);
-                setLoadingModal(false); // Also set loading to false in case of error
+                setLoadingModal(false);
             });
     }, []);
 
-    /**
-     * handleProcessData: Callback to handle and append new data from the child process to the output.
-     * It updates the state by appending the new data received to the existing output.
-     * @param {string} data - The data received from the child process.
-     */
     const handleProcessData = useCallback((data: string) => {
-        setOutput((prevOutput) => prevOutput + "\n" + data); // Append new data to the previous output.
+        setOutput((prevOutput) => prevOutput + "\n" + data);
     }, []);
 
-    /**
-     * handleProcessTermination: Callback to handle the termination of the child process.
-     * Once the process termination is handled, it clears the process PID reference and
-     * deactivates the loading overlay.
-     * @param {object} param - An object containing information about the process termination.
-     * @param {number} param.code - The exit code of the terminated process.
-     * @param {number} param.signal - The signal code indicating how the process was terminated.
-     */
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
-            // If the process was successful, display a success message.
             if (code === 0) {
                 handleProcessData("\nProcess completed successfully.");
-
-                // If the process was terminated manually, display a termination message.
             } else if (signal === 15) {
                 handleProcessData("\nProcess was manually terminated.");
-
-                // If the process was terminated with an error, display the exit and signal codes.
             } else {
                 handleProcessData(`\nProcess terminated with exit code: ${code} and signal code: ${signal}`);
             }
-
-            // Clear the child process pid reference. There is no longer a valid process running.
             setPid("");
-
-            // Cancel the loading overlay. The process has completed.
             setLoading(false);
         },
-        [handleProcessData] // Dependency on the handleProcessData callback
+        [handleProcessData]
     );
 
-    /**
-     * Handles form submission for the Wifite component.
-     * @param {FormValuesType} values - The form values containing the target domain.
-     */
     const onSubmit = async (values: FormValuesType) => {
-        // Activate loading state to indicate ongoing process
         setLoading(true);
 
-        // Construct arguments for the Wifite command based on form input
         let args = [];
         args = [values.targetInterface, "-a", values.attackMode];
 
-        // Check if channel has a value and push it to args
         if (values.channel) {
             args.push("--channel", values.channel);
         }
 
         if (verboseMode) {
-            args.push("-v"); // Add verbose mode option if enabled
+            args.push("-v");
         }
 
-        // Execute the Wifite command via helper method and handle its output or potential errors
         CommandHelper.runCommandWithPkexec("wifite", args, handleProcessData, handleProcessTermination)
             .then(({ output, pid }) => {
-                // Deactivate loading state
-                // Update the UI with the results from the executed command
                 setOutput(output);
-                setAllowSave(true); // Allow saving the output
+                setAllowSave(true);
                 setPid(pid);
             })
             .catch((error) => {
-                // Display any errors encountered during command execution
                 setOutput(`Error: ${error.message}`);
-                // Deactivate loading state
                 setLoading(false);
             });
     };
 
-    // Render component
     return (
         <RenderComponent
             title={title}
