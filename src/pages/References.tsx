c3d4af2514cfa317b10b81d331eb86009765b92a
s218325308 <162116837+s218325308@users.noreply.github.com>
2024-11-23 21:14:41 +1000

    Update References.tsx
    Fix errors found due to failed style test

--- src/pages/References.tsx
+++ src/pages/References.tsx
@@ -89,12 +89,9 @@ const ReferencesPage = () => {
                                 />
                                 <Reference
                                     name={"Arpaname"}
-
                                     description={
                                         "Translate IP addresses to the corresponding ARPA names."
                                     }
-                                    description={"Translate IP addresses to the corresponding ARPA names."}
-
                                     url={"https://www.kali.org/tools/bind9/#arpaname"}
                                 />
                                 <Reference
@@ -307,7 +304,6 @@ const ReferencesPage = () => {
                                     description={
                                         "JohnTheRipper is a free and open source password cracking tool."
                                     }
-                                    description={"JohnTheRipper is a free and open source password cracking tool."}
                                     url={"https://www.openwall.com/john"}
                                 />
                                 <Reference
@@ -532,7 +528,6 @@ const ReferencesPage = () => {
                                     description={
                                         "WhatWeb identifies websites."
                                     }
-                                    description={"WhatWeb identifies websites."}
                                     url={"https://www.kali.org/tools/whatweb/"}
                                 />
                                 <Reference
@@ -540,7 +535,6 @@ const ReferencesPage = () => {
                                     description={
                                         "Whois is a command-line tool used to query databases."
                                     }
-                                    description={"Whois is a command-line tool used to query databases."}
                                     url={"https://www.kali.org/tools/whois/"}
                                 />
                                 <Reference
@@ -548,7 +542,6 @@ const ReferencesPage = () => {
                                     description={
                                         "Wifite is a tool to audit WEP or WPA encrypted wireless networks."
                                     }
-                                    description={"Wifite is a tool to audit WEP or WPA encrypted wireless networks"}
                                     url={"https://www.kali.org/tools/wifite/"}
                                 />
                                 <Reference
