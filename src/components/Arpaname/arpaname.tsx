69f4e2a6883bf85c3a0d50e83a2bbc2750598cc1
Harmit3703 <harmitdalwadi@gmail.com>
2024-11-29 10:31:48 +0530

    Add switch for IPv4 and IPv6 in Arpaname tool

--- src/components/Arpaname/arpaname.tsx
+++ src/components/Arpaname/arpaname.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import { Button, Stack, TextInput } from "@mantine/core";
+import { Button, Stack, TextInput, Radio } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import ConsoleWrapper from "../ConsoleWrapper/ConsoleWrapper";
 import { RenderComponent } from "../UserGuide/UserGuide";
@@ -14,6 +14,7 @@ import { SaveOutputToTextFile_v2 } from "../SaveOutputToFile/SaveOutputToTextFil
  */
 interface FormValuesType {
     ipAddress: string;
+    ipType: "IPv4" | "IPv6"; // New field to store the IP type
 }
 
 /**
@@ -23,25 +24,25 @@ interface FormValuesType {
  * @returns JSX.Element The rendered ArpanameTool component
  */
 const ArpanameTool = () => {
-    const title = "Arpaname"; // Title of the tool displayed in the UI
-    const description = "Perform reverse DNS lookups for IP addresses."; // Brief description of the tool's functionality
-    const [loading, setLoading] = useState(false); // State variable to track if the process is currently loading
-    const [output, setOutput] = useState(""); // State variable to store the output of the process
-    const [pidTarget, setPidTarget] = useState(""); // State variable to store the PID of the target process.
-    const [isCommandAvailable, setIsCommandAvailable] = useState(false); // State variable to check if the command is available.
-    const [opened, setOpened] = useState(!isCommandAvailable); // State variable that indicates if the modal is opened.
-    const [loadingModal, setLoadingModal] = useState(true); // State variable to indicate loading state of the modal.
-    const [errorMessage, setErrorMessage] = useState<string>(""); // State variable to store the error message
-    const [allowSave, setAllowSave] = useState(false); // State variable to allow saving the output to a file.
-    const [hasSaved, setHasSaved] = useState(false); // State variable to indicate if the output has been saved.
+    const title = "Arpaname";
+    const description = "Perform reverse DNS lookups for IP addresses.";
+    const [loading, setLoading] = useState(false);
+    const [output, setOutput] = useState("");
+    const [pidTarget, setPidTarget] = useState("");
+    const [isCommandAvailable, setIsCommandAvailable] = useState(false);
+    const [opened, setOpened] = useState(!isCommandAvailable);
+    const [loadingModal, setLoadingModal] = useState(true);
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [allowSave, setAllowSave] = useState(false);
+    const [hasSaved, setHasSaved] = useState(false);
 
-    // Component Constants.
+    // Component Constants
     const steps =
         "Step 1: Type in the target IP address\n" +
         "Step 2: Click lookup to run Arpaname.\n" +
-        "Step 3: View the output block to see the results. ";
-    const sourceLink = "https://www.kali.org/tools/bind9/#arpaname"; // Link to the source code (or Kali Tools).
-    const tutorial = ""; // Link to the official documentation/tutorial.
+        "Step 3: View the output block to see the results.";
+    const sourceLink = "https://www.kali.org/tools/bind9/#arpaname";
+    const tutorial = "";
     const dependencies = ["arpaname"];
 
     // Check for command availability on component mount
@@ -55,21 +56,20 @@ const ArpanameTool = () => {
             })
             .catch((error) => {
                 console.error("An error occurred:", error);
-                setLoadingModal(false); // Ensure loading state is reset even if an error occurs
+                setLoadingModal(false);
             });
     }, []);
 
     const form = useForm<FormValuesType>({
         initialValues: {
             ipAddress: "",
+            ipType: "IPv4", // Default type is IPv4
         },
     });
 
     /**
     handleProcessData: Callback to handle and append new data from the child process to the output.
-    It updates the state by appending the new data received to the existing output.
-    @param {string} data - The data received from the child process.
-    */
+    It updates the state by appending the new data received to the existing output.*/
     const handleProcessData = useCallback((data: string) => {
         setOutput((prevOutput) => prevOutput + "\n" + data);
     }, []);
@@ -77,11 +77,7 @@ const ArpanameTool = () => {
     /**
     handleProcessTermination: Callback to handle the termination of the child process.
     Once the process termination is handled, it clears the process PID reference and
-    deactivates the loading overlay.
-    @param {object} param - An object containing information about the process termination.
-    @param {number} param.code - The exit code of the terminated process.
-    @param {number} param.signal - The signal code indicating how the process was terminated.
-    */
+    deactivates the loading overlay.*/
     const handleProcessTermination = useCallback(
         ({ code, signal }: { code: number; signal: number }) => {
             if (code === 0) {
@@ -110,24 +106,17 @@ const ArpanameTool = () => {
         setAllowSave(false); // Disable the save option after successful save
     };
 
-    /**
-     * Validates if the given input string is a valid IPv4 or IPv6 address.
-     * @param {string} ip - The IP address string to be validated.
-     * @returns {boolean} True if the input is a valid IP address, false otherwise.
-     */
-    const validateIPAddress = (ip: string) => {
+    // Validates if the given input string is a valid IPv4 or IPv6 address.
+    const validateIPAddress = (ip: string, type: "IPv4" | "IPv6") => {
         const ipv4Pattern = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-        const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4}|:)$/;
-        return ipv4Pattern.test(ip) || ipv6Pattern.test(ip);
+        const ipv6Pattern =
+            /^(([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}|(([0-9a-fA-F]{1,4}:){0,6}:([0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4})|::)$/;
+        return type === "IPv4" ? ipv4Pattern.test(ip) : ipv6Pattern.test(ip);
     };
 
-    /**
-     * Async function to handle form submission and execute the arpaname command.
-     * @param {FormValuesType} values - The form values containing the IP address.
-     */
     const onSubmit = async (values: FormValuesType) => {
-        if (!validateIPAddress(values.ipAddress)) {
-            setErrorMessage("Please enter a valid IP address."); // Set error message for invalid IP
+        if (!validateIPAddress(values.ipAddress, values.ipType)) {
+            setErrorMessage(`Please enter a valid ${values.ipType} address.`);
             return;
         }
         // Disallow saving until the tool's execution is complete
@@ -138,7 +127,6 @@ const ArpanameTool = () => {
         setLoading(true);
 
         const argsIP = [values.ipAddress]; // Prepare arguments for the arpaname command
-
         // Execute arpaname command for the target
         const result_target = await CommandHelper.runCommandGetPidAndOutput(
             "arpaname",
@@ -181,7 +169,16 @@ const ArpanameTool = () => {
                 <Stack>
                     {LoadingOverlayAndCancelButton(loading, pidTarget)}
                     <TextInput label={"IP address"} required {...form.getInputProps("ipAddress")}></TextInput>
-                    {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}{" "}
+                    <Radio.Group
+                        value={form.values.ipType}
+                        onChange={(value) => form.setFieldValue("ipType", value as "IPv4" | "IPv6")}
+                        label="Select IP Type"
+                        required
+                    >
+                        <Radio value="IPv4" label="IPv4" />
+                        <Radio value="IPv6" label="IPv6" />
+                    </Radio.Group>
+                    {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
                     {/* Render error message if present */}
                     <Button type={"submit"}>Lookup</Button>
                     {SaveOutputToTextFile_v2(output, allowSave, hasSaved, handleSaveComplete)}
