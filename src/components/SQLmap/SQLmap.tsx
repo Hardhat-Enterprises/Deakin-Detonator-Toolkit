08251de319665adfb6ae8549f95c5ec7461d8eb8
sarthakghildiyal <sarthakghildiyal@gmail.com>
2026-01-20 19:25:48 +1100

    Add SQLmap tutorial documentation for PT-GUI

--- src/components/SQLmap/SQLmap.tsx
+++ src/components/SQLmap/SQLmap.tsx
@@ -45,7 +45,8 @@ function SQLmap() {
         "Step 2: Start the process.\n" +
         "Step 3: Review the output for further analysis.\n";
     const sourceLink = "https://github.com/sqlmapproject/sqlmap"; // Link to the source code
-    const tutorial = "https://docs.google.com/document/d/1O0yp3_vazO2UsnUBcbSyeuYJXq9x7Vcsjjsfw_LLhEc/edit?usp=sharing"; // Link to the official documentation/tutorial
+    const tutorial =
+        "https://docs.google.com/document/d/1CqxVDNGUw1LTVkB0dgsE9xiyzJksLZ3Jh1u_UGb0N0A/edit?pli=1&tab=t.0#heading=h.ytpj5ebu7b6l"; // Link to the official documentation/tutorial
     const dependencies = ["sqlmap"]; // Contains the dependencies required by the component.
 
     // Form hook to handle form input
