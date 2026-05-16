eb54978ea31fb357455e276c39a833b564cbd29e
joabjunxianglow <s222177749@deakin.edu.au>
2026-05-16 06:12:31 -0400

    improve-shodanAPI-tool

--- src/components/Shodan/Shodan-API-Tool.tsx
+++ src/components/Shodan/Shodan-API-Tool.tsx
@@ -39,23 +39,23 @@ export function ShodanAPITool() {
         "The Shodan API is a powerful tool that allows external network scans to be performed with use of a valid API key."; // Description of the component.
     const steps =
         "How to use Shodan API:\n" +
-        "Step 1: Install shodkey.py to /usr/share/ddt by running install_exploits.sh script or manually transfer shodkey.py to /usr/share/ddt/\n" +
-        "Step 2: Enter a valid API Key\n" +
+        "Step 1: Obtain a valid API key by creating an account at https://account.shodan.io; once signed in, the API should be within the account's overview\n" + 
+        "Step 2: Enter the obtained API Key\n" +
         "Step 3: Select the desired Shodan API endpoint.\n" +
         "Step 4: Depending on the endpoint, enter a Host IP or a Search Query.\n" +
         "Step 5: Click Scan button to commence the Shodan API operation. Or click Cancel Scan to terminate scan\n" +
         "Step 6: View the Output block below to view the results of the tool's execution.\n" +
         "Step 7: Optional: to save scan results enter filename and click on the save output to file button";
     const sourceLink = "https://developer.shodan.io/api"; // Link to the source code.
-    const tutorial = "https://docs.google.com/document/d/1ClC_WmlK8ua6rdpYUE4LsQDOmUQJQf1PFVWVW7uu2jM/edit?usp=sharing"; // Link to the official documentation/tutorial.
-    const dependencies = ["python3"]; // Contains the dependencies required by the component.
+    const tutorial = "https://docs.google.com/document/d/1doC-ru2Ivvqx925en0SzU5E-ROQRufdi0MftiozhKgo/edit?usp=sharing"; // Link to the official documentation/tutorial.
+    const dependencies = ["shodan"]; // Contains the dependencies required by the component.
 
     // Form Hook to handle form input.
     let form = useForm({
         initialValues: {
             hostIP: "",
             shodanKey: "",
-            endpoint: "host", // Default endpoint
+            endpoint: "", // Default endpoint
             searchQuery: "",
         },
     });
@@ -147,20 +147,21 @@ export function ShodanAPITool() {
         setLoading(true);
 
         // Construct the arguments array dynamically based on the selected endpoint
-        const args = ["/usr/share/ddt/shodkey.py", "-k", values.shodanKey, "-e", values.endpoint];
+        const args = [values.endpoint];
 
         if (requiresIP) {
-            args.push("-i", values.hostIP);
+            args.push(values.hostIP);
         }
 
         if (requiresQuery) {
-            args.push("-q", values.searchQuery);
+            args.push(values.searchQuery);
         }
 
         try {
             // Execute the Shodan command via helper method and handle its output or potential errors.
+            CommandHelper.runCommand("shodan", values.shodanKey) //initialises the Shodan CLI with the obtained API key
             const result = await CommandHelper.runCommandGetPidAndOutput(
-                "python3",
+                "shodan",
                 args,
                 handleProcessData,
                 handleProcessTermination
@@ -209,11 +210,9 @@ export function ShodanAPITool() {
                         label="Select Shodan API Endpoint"
                         placeholder="Choose endpoint"
                         data={[
-                            { value: "host", label: "/shodan/host" },
-                            { value: "host/count", label: "/shodan/host/count" },
-                            { value: "host/search", label: "/shodan/host/search" },
-                            { value: "host/search/facets", label: "/shodan/host/search/facets" },
-                            { value: "host/search/tokens", label: "/shodan/host/search/tokens" },
+                            { value: "host", label: "host" },
+                            { value: "count", label: "count" },
+                            { value: "search", label: "earch" },
                         ]}
                         required
                         {...form.getInputProps("endpoint")}
