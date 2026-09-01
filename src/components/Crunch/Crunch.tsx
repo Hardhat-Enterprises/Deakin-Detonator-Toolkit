eecc4ef39dde9848c73498313517ba8efae10e06
Sachin Kumar <sachinkumarsk3303@gmail.com>
2026-09-01 07:54:37 -0400

    Fix Crunch dependency detection

--- src/components/Crunch/Crunch.tsx
+++ src/components/Crunch/Crunch.tsx
@@ -54,7 +54,7 @@ const Crunch = () => {
 
     const sourceLink = "https://www.kali.org/tools/crunch/"; // Link to the source code (or Kali Tools).
     const tutorial = "https://docs.google.com/document/d/1NoYLod8jyXOLAIUGU-d7Zeq_-_XDzXO-XzADKLPk72I/edit?usp=sharing"; // Link to the official documentation/tutorial.
-    const dependencies = ["Crunch"]; // Contains the dependencies required by the component.
+    const dependencies = ["crunch"]; // Contains the dependencies required by the component.
 
     //initial form values
     //Update initial value for minLength = 3, maxLength = 4, charset = abcde
